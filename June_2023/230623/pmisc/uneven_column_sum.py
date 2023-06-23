c4df596f234140bf0bfae78f7f5c55899e65eafc
heyhenry <henryyy3@hotmail.com>
2023-06-23 20:58:06 +0800

    Solved problem that addresses ability to take in uneven column lengths.

--- June_2023/230623/pmisc/uneven_column_sum.py
+++ June_2023/230623/pmisc/uneven_column_sum.py
@@ -9,21 +9,52 @@ column_sum([[2, 2], [-1, -2], [3, 9]]) -> [4, 9]
 """
 def uneven_column_sum(mat : list[list[int]]) -> list[int]:
 
+    # finds the number of cols in each row and puts them in a list
+    col_list = []
+
+    for row in mat:
+        value = len(row)
+        col_list.append(value)
+
+    # print(col_list)
+
+    # finds the highest number out of the list of column numbers for each row
+    highest_col_num = col_list[0]
+
+    for i in col_list:
+        if i > highest_col_num:
+            highest_col_num = i
+
+    # print(highest_col_num)            
+
+    # checks each row's length and adds 0s to those that are not equal length to the largest row
+    for row in mat:
+        while len(row) != highest_col_num:
+            row.append(0)
+
+    updated_mat = []
+    updated_mat += mat
+
+    # for row in updated_mat:
+    #     print(len(row))
+
+    # print(updated_mat)
+
     results = []
 
-    for col in range(len(mat[0])):
+    for col in range(len(updated_mat[0])):
         sum_value = 0
-        for row in range(len(mat)):
-            sum_value += mat[row][col]
+        for row in range(len(updated_mat)):
+            sum_value += updated_mat[row][col]
         results.append(sum_value)
-
-    return results   
+  
+    return results
 
 def main():
-    # print(column_sum([[1, 2, 3], [4, 5, 6]]))
-    # print(column_sum([[], []]))
-    # print(column_sum([[2, 2], [-1, -2], [3, 9]]))
-    print(uneven_column_sum([[2, 2], [5]]))
+    # print(uneven_column_sum([[1, 2, 3], [4, 5, 6]]))
+    # print(uneven_column_sum([[], []]))
+    # print(uneven_column_sum([[2, 2], [-1, -2], [3, 9]]))
+    print(uneven_column_sum([[2, 2], [5], [1, 2, 3]])) # output -> [8, 4, 3]
 
 if __name__ == "__main__":
     main()
