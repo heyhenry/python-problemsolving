f826a88d63f45132cdb98e17d4d62d812197672a
heyhenry <henryyy3@hotmail.com>
2023-06-23 20:25:23 +0800

    Solved 99% of pset_01 problems.

---
+++ June_2023/230623/pset_02/expansion.py
@@ -0,0 +1,25 @@
+"""
+Returns the list of each letter of `s` expanded in sequence
+Examples:
+expansion('test') -> ['t', 'te', 'tes', 'test']
+expansion('') -> []
+expansion('abccba') -> ['a', 'ab', 'abc', 'abcc', 'abccb', 'abccba']
+"""
+def expansion(s : str) -> list[str]:
+
+    gibberish = ''
+    results = []
+
+    for c in s:
+        gibberish += c
+        results.append(gibberish)
+
+    return results
+
+def main():
+    print(expansion('test'))
+    print(expansion(''))
+    print(expansion('abccba'))
+
+if __name__ == "__main__":
+    main()
