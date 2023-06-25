ffb96f2200b96e0e79013356edde885de714eaec
heyhenry <henryyy3@hotmail.com>
2023-06-26 06:32:49 +0800

    Completed rest of pset_02 problems.

--- June_2023/260623/pset_02/row_sum.py
+++ June_2023/260623/pset_02/row_sum.py
@@ -1,4 +1,3 @@
-def row_sum(table : list[list[int]]) -> list[list[int]]:
 """
 Returns the sum up to each element of the given table
 Examples:
@@ -7,3 +6,25 @@ row_sum([[], [4], [], [5]]) -> [[], [4], [], [5]]
 row_sum([[-1, -5, 6], [7, 2], [10, 10, 10]]) -> [[-1, -6, 0], [7, 9], [10, 20,
 30]]
 """
+def row_sum(table : list[list[int]]) -> list[list[int]]:
+
+    results = []
+
+    for row in table:
+        temp = []
+        sum_value = 0
+        for i in row:
+            sum_value += i
+            print(sum_value)
+            temp.append(sum_value)
+        results.append(temp)
+
+    return results
+
+def main():
+    print(row_sum([[1, 2, 3], [4, 5, 6]]))
+    print(row_sum([[], [4], [], [5]]))
+    print(row_sum([[-1, -5, 6], [7, 2], [10, 10, 10]]))
+
+if __name__ == "__main__":
+    main()
--- June_2023/260623/pset_02/staggered_sum.py
+++ June_2023/260623/pset_02/staggered_sum.py
@@ -1,4 +1,3 @@
-def staggered_sum(lst : list[int | list[int]]) -> int:
 """
 Returns the sum of elements in `lst`, which is a list of integers _or_ lists
 Examples:
@@ -7,3 +6,24 @@ staggered_sum([[], [], []]) -> 0
 staggered_sum([[1, 2], 3, [4, 5]]) -> 15
 staggered_sum([-4, [-3, 2, 1], 6, 8, [-1]]) -> 9
 """
+def staggered_sum(lst : list[int | list[int]]) -> int:
+
+    sum_value = 0
+
+    for row_or_i in lst:
+        if isinstance(row_or_i, list):
+            for i in row_or_i:
+                sum_value += i
+        else:
+            sum_value += row_or_i
+
+    return sum_value
+
+def main():
+    print(staggered_sum([1, 2, 3]))
+    print(staggered_sum([[], [], []]))
+    print(staggered_sum([[1, 2], 3, [4, 5]]))
+    print(staggered_sum([-4, [-3, 2, 1], 6, 8, [-1]]))
+
+if __name__ == "__main__":
+    main()
