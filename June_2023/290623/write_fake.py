0df5b7cad5b20c45a18802d1d418bc5548784427
heyhenry <henryyy3@hotmail.com>
2023-06-29 23:21:20 +0800

    Completed fakeletters problems.

---
+++ June_2023/290623/write_fake.py
@@ -0,0 +1,44 @@
+"""
+Writes the given list of strings to the `.fakeletters` file
+Each word must consist of only letters, and will be written
+to be all lowercase and space-separated
+(Note, you can use the previous two functions to help test this one!)
+Requires that `filename` ends with `.fakeletters`
+Example:
+write_fake("output.fakeletters", ["test", "out"])
+contains("output.fakeletters", "test") -> True
+contains("output.fakeletters", "other") -> False
+count_words("output.fakeletters") -> 2
+"""
+from count_words import count_words
+from contains import contains
+def write_fake(filename : str, words : list[str]):
+
+    given_extension = filename[-12:]
+    valid_extension = '.fakeletters'
+
+    if given_extension == valid_extension:
+
+        with open(filename, 'w') as file:
+
+            for w in range(len(words)):
+
+                if w != (len(words) - 1):
+                    file.write(words[w])
+                    file.write(' ')
+                else:
+                    file.write(words[w])
+
+    return 'Wrong extension provided. Please use .fakeletters'
+
+def main():
+    # write_fake("fakeletters/output.fakeletters", ["test", "out"])
+
+    # for testing
+    print(write_fake("fakeletters/output.fakeletters", ["test", "out"]))
+    print(contains("fakeletters/output.fakeletters", "test"))
+    print(contains("fakeletters/output.fakeletters", "other"))
+    print(count_words("fakeletters/output.fakeletters"))
+
+if __name__ == "__main__":
+    main()
