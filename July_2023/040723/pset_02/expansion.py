2e494aa54105ef60eb4d38fcc62499bd02f05a3e
heyhenry <henryyy3@hotmail.com>
2023-07-04 07:39:49 +0800

    Completed pset_02.

---
+++ July_2023/040723/pset_02/expansion.py
@@ -0,0 +1,25 @@
+"""
+Returns the list of each letter of `s` expanded in sequence
+Examples:
+expansion('test') -> ['t', 'te', 'tes', 'test']
+expansion('') -> []
+expansion('abccba') -> ['a', 'ab', 'abc', 'abcc', 'abccb', 'abccba']
+"""
+def expansion(s : str) -> list[str]:
+
+    exp = ''
+    results = []
+
+    for c in s:
+        exp += c
+        results.append(exp)
+
+    return results
+
+def main():
+    print(expansion('test'))
+    print(expansion(''))
+    print(expansion('abccba'))
+
+if __name__ == "__main__":
+    main()
---
+++ July_2023/040723/pset_02/max_even_table.py
@@ -0,0 +1,31 @@
+"""
+Returns the maximum even number in each row of `lst`, or `None` if none exists
+Note that negatives will be negative when the associated positive is
+Examples:
+max_even_table([[1, 2, 3]]) -> [2]
+max_even([[-4, 3, -1, -6], [1, 3, 5]) -> [-4, None]
+max_even_table([[], [1, 4, 6, 2], [-1, -2]]) -> [None, 6, -2]
+"""
+def max_even_table(lst : list[list[int]]) -> list[int | None]:
+
+    results = []
+
+    for row in lst:
+        mnum = None
+        for i in row:
+            if i % 2 == 0:
+                if mnum is None:
+                    mnum = i
+                elif i > mnum: 
+                    mnum = i
+        results.append(mnum)
+
+    return results
+
+def main():
+    print(max_even_table([[1, 2, 3]]))
+    print(max_even_table([[-4, 3, -1, -6], [1, 3, 5]]))
+    print(max_even_table([[], [1, 4, 6, 2], [-1, -2]]))
+
+if __name__ == "__main__":
+    main()
