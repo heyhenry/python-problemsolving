fb115344c6d66748b73ab330b74adf85ef04c0bc
heyhenry <henryyy3@hotmail.com>
2023-07-12 17:06:11 +0800

    Completed pset_1 problems.

---
+++ July_2023/120723/pset_01/second_vowel.py
@@ -0,0 +1,32 @@
+"""
+Returns the index of the second vowel in `s`
+If no vowels are in `s`, returns -1
+A vowel is defined to be 'aeiou', and a 'y' that _isn't_ the first character
+Examples:
+second_vowel('test') -> -1
+second_vowel('aeiou') -> 1
+second_vowel('slowly') -> 5
+second_vowel('yip') -> -1
+"""
+def second_vowel(s : str) -> int:
+
+    counter = 0
+
+    for c in range(len(s)):
+        if s[c] in 'aeiou' or s[c] == 'y' and c != 0:
+            counter += 1
+        if counter == 2:
+            return c
+
+    return -1
+
+def main():
+    print(second_vowel('test'))
+    print(second_vowel('aeiou'))
+    print(second_vowel('slowly'))
+    print(second_vowel('yip'))
+    print(second_vowel('yppy'))
+    print(second_vowel('pygmy'))
+
+if __name__ == "__main__":
+    main()
