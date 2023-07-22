bbf166e756598b8674d7c44e75de3f43d8915da6
heyhenry <henryyy3@hotmail.com>
2023-07-22 12:39:40 +0800

    Completed pset_01 and pset_02.

---
+++ July_2023/220723/pset_01/reverse_string.py
@@ -0,0 +1,23 @@
+"""
+Returns the string `s` in reverse order
+Examples:
+reverse_string('abc') -> 'cba'
+reverse_string('') -> ''
+reverse_string('abab') -> 'baba'
+"""
+def reverse_string(s : str) -> str:
+
+    txt = ''
+
+    for c in s:
+        txt = c + txt
+
+    return txt
+
+def main():
+    print(reverse_string('abc'))
+    print(reverse_string(''))
+    print(reverse_string('abab'))
+
+if __name__ == "__main__":
+    main()
