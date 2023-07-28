182e4eb73a2ac0356d6e2f4f7f1d47c28d27d176
heyhenry <henryyy3@hotmail.com>
2023-07-28 15:12:23 +0800

    solved more pset problems.

---
+++ July_2023/280723/pset_02/double.py
@@ -0,0 +1,39 @@
+
+"""
+Returns nothing
+Modifies table in place to double each number
+Examples:
+double([[1, 2], [3, 4, 5]]) changes lst to [[2, 4], [6, 8, 10]]
+double([[-1], [-2], [0, 0]]) changes lst to [[-2], [-4], [0, 0]]
+double([[]]) does "nothing"
+"""
+def double(table : list[list[int]]):
+
+    updated_table = []
+
+    for row in table:
+        temp = []
+        for i in row:
+            temp.append(i * 2)
+        updated_table.append(temp)
+
+    table.clear() 
+
+    table += updated_table
+
+def main():
+    
+    lst1 = [[1, 2], [3, 4, 5]]
+    lst2 = [[-1], [-2], [0, 0]]
+    lst3 = [[]]
+    
+    double(lst1)
+    double(lst2)
+    double(lst3)
+
+    print(lst1)
+    print(lst2)
+    print(lst3)
+
+if __name__ == "__main__":
+    main()
