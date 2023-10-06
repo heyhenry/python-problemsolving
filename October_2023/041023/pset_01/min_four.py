2d0f81df9dead5efea6164cb981a374df358a2af
heyhenry <henryyy3@hotmail.com>
2023-10-06 23:57:17 +0800

    Python problem solving pset01

---
+++ October_2023/041023/pset_01/min_four.py
@@ -0,0 +1,19 @@
+"""
+Returns the minimum of the four given integers
+Examples:
+min_four(1, 2, 3, 4) -> 1
+min_four(3, 2, 4, 6) -> 2
+min_four(0, 0, -5, 0) -> -5
+"""
+def min_four(x : int, y : int, z : int, w : int) -> int:
+
+    return min(x, y, z, w)
+
+def main():
+
+    print(min_four(1, 2, 3, 4))
+    print(min_four(3, 2, 4, 6))
+    print(min_four(0, 0, -5, 0))
+
+if __name__ == "__main__":
+    main()
---
+++ October_2023/041023/pset_01/reverse_string.py
@@ -0,0 +1,23 @@
+"""
+Returns the string `s` in reverse order
+Examples:
+reverse_string('abc') -> 'cba'
+reverse_string('') -> ''
+reverse_string('abab') -> 'baba'
+"""
+def reverse_string(s : str) -> str:
+
+    result = ''
+
+    for c in s:
+        result = c + result
+
+    return result
+
+def main():
+    print(reverse_string('abc'))
+    print(reverse_string(''))
+    print(reverse_string('abab'))
+
+if __name__ == '__main__':
+    main()
