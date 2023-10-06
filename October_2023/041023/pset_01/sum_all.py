b962b699b04f0de613371e7a604db01463244735
heyhenry <henryyy3@hotmail.com>
2023-10-07 00:17:15 +0800

    New way to intepret the sum_all problem.

--- October_2023/041023/pset_01/sum_all.py
+++ October_2023/041023/pset_01/sum_all.py
@@ -8,20 +8,30 @@ sum_all([[]]) -> []
 def sum_all(table : list[list[int]]) -> list[int]:
 
     result = []
+    isFullEmpty = False
+    counter = 0
 
     for i in table:
         sum_value = 0
         for x in i:
             sum_value += x
         result.append(sum_value)
+        if len(i) == 0:
+            counter += 1
+            if counter == len(table):
+                isFullEmpty = True
 
-    return result
+    if isFullEmpty:
+        return []
+    else:
+        return result
 
 def main():
 
     print(sum_all([[1, 2, 3], [4, 5, 6]]))
     print(sum_all([[2], [4, 7], [-1, -2, -4, -5]]))
-    print(sum_all([[]]))
+    print(sum_all([[]])) # []
+    print(sum_all([[], [2, 7]])) # [], [9]
 
 if __name__ == "__main__":
     main()
