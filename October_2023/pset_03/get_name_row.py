873eafd1da5a558b1bed14e055decee2f7ea1e4e
heyhenry <henryyy3@hotmail.com>
2023-10-17 13:50:49 +0800

    Solved get_name_row.py from pset_03.

--- October_2023/pset_03/get_name_row.py
+++ October_2023/pset_03/get_name_row.py
@@ -5,12 +5,12 @@ If the table has no `total` column or `name` is not found, returns -1
 If the file contains `first name` and `last name` rather than `name`
 Then this function should treat those as concatenated by a space
 Examples:
-get_name_total("data1.csv", "goblin") -> -1 // no total
-get_name_total("data2.csv", "bob smith") -> 1234
-get_name_total("data2.csv", "alice jones") -> 6712
-get_name_total("data3.csv", "Calvin Harris") -> 64953382
-get_name_total("data3.csv", "The Weeknd") -> 108390904
-get_name_total("data3.csv", "Test") -> -1
+get_name_total("table1.csv", "goblin") -> -1 // no total
+get_name_total("table2.csv", "bob smith") -> 1234
+get_name_total("table2.csv", "alice jones") -> 6712
+get_name_total("table3.csv", "Calvin Harris") -> 64953382
+get_name_total("table3.csv", "The Weeknd") -> 108390904
+get_name_total("table3.csv", "Test") -> -1
 """
 def get_name_total(filename : str, name : str) -> int:
 
@@ -30,36 +30,66 @@ def get_name_total(filename : str, name : str) -> int:
                 for line in content_splitlines:
                     content.append(line.split(','))
 
-                containsTotal = False
-                containsName = False
-
+                name_pos = 0
+                total_pos = 0
                 fname_pos = 0
                 lname_pos = 0
-                total_pos = 0
 
+                contains_total = False
+                contains_name = False
+                contains_fname = False
+                contains_lname = False
+                
+                # checks name type column
+                for i in range(len(content[0])):
+                    if content[0][i] == 'name' and len(content[0][i]) == 4:
+                        name_pos = i
+                        contains_name = True
+                    if content[0][i] == 'total' and len(content[0][i]) == 5:
+                        total_pos = i
+                        contains_total = True
+
+                # checks first name, last name type columns
                 for i in range(len(content[0])):
                     if content[0][i] == 'first name' and len(content[0][i]) == 10:
+                        fname_pos = i
+                        contains_fname = True
                         
-                        if content[0][i] == 'last name' and len(content[0][i]) == 9:
-                            containsName = True
-                            if content[0][i] == 'total' and len(content[0][i]) == 5:
-                                containsTotal = True
-                
-            
-                
+                for i in range(len(content[0])):
+                    if content[0][i] == 'last name' and len(content[0][i]) == 9:
+                        lname_pos = i
+                        contains_lname = True
+                    if content[0][i] == 'total' and len(content[0][i]) == 5:
+                        total_pos = i
+                        contains_total = True
 
+                if contains_name and contains_total:
+                    
+                    for row in range(len(content)):
+                        if content[row][name_pos] == name:
+                            return content[row][total_pos]
+
+                elif contains_fname and contains_lname and contains_total:
+                    
+                    full_name = name.split(' ')    
+
+                    for row in range(len(content)):
+                        if content[row][fname_pos] == full_name[0] and content[row][lname_pos] == full_name[1]:
+                            return content[row][total_pos]
+                return -1
+            
             return -1
 
     return -1
 
 def main():
     
-    print(get_name_total("tables/data1.csv", "goblin"))
-    print(get_name_total("tables/data2.csv", "bob smith"))
-    print(get_name_total("tables/data2.csv", "alice jones"))
-    print(get_name_total("tables/data3.csv", "Calvin Harris"))
-    print(get_name_total("tables/data3.csv", "The Weeknd"))
-    print(get_name_total("tables/data3.csv", "Test"))
+    print(get_name_total("tables/table1.csv", "goblin"))
+    print(get_name_total("tables/table2.csv", "bob smith"))
+    print(get_name_total("tables/table2.csv", "alice jones"))
+    print(get_name_total("tables/table3.csv", "Calvin Harris"))
+    print(get_name_total("tables/table3.csv", "The Weeknd"))
+    print(get_name_total("tables/table3.csv", "Test"))
 
 if __name__ == "__main__":
     main()
