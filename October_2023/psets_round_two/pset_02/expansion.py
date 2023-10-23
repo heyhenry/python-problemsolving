99db83586b731f5bf298f240a9260a3028af99d7
heyhenry <henryyy3@hotmail.com>
2023-10-24 07:39:07 +0800

    Solved some problems from pset_02.

---
+++ October_2023/psets_round_two/pset_02/expansion.py
@@ -0,0 +1,25 @@
+"""
+Returns the list of each letter of `s` expanded in sequence
+Examples:
+expansion('test') -> ['t', 'te', 'tes', 'test']
+expansion('') -> []
+expansion('abccba') -> ['a', 'ab', 'abc', 'abcc', 'abccb', 'abccba']
+"""
+def expansion(s : str) -> list[str]:
+
+    result = []
+    word = ''
+
+    for c in s:
+        word += c
+        result.append(word)
+
+    return result
+
+def main():
+    print(expansion('test'))
+    print(expansion(''))
+    print(expansion('abccba'))
+
+if __name__ == "__main__":
+    main()
---
+++ October_2023/psets_round_two/pset_02/row_sum.py
@@ -0,0 +1,29 @@
+"""
+Returns the sum up to each element of the given table
+Examples:
+row_sum([[1, 2, 3], [4, 5, 6]]) -> [[1, 3, 6], [4, 9, 15]]
+row_sum([[], [4], [], [5]]) -> [[], [4], [], [5]]
+row_sum([[-1, -5, 6], [7, 2], [10, 10, 10]]) -> [[-1, -6, 0], [7, 9], [10, 20,
+30]]
+"""
+def row_sum(table : list[list[int]]) -> list[list[int]]:
+
+    result = []
+
+    for row in table:
+        sum_val = 0
+        temp = []
+        for i in row:
+            sum_val += i
+            temp.append(sum_val)
+        result.append(temp)
+
+    return result
+
+def main():
+    print(row_sum([[1, 2, 3], [4, 5, 6]]))
+    print(row_sum([[], [4], [], [5]]))
+    print(row_sum([[-1, -5, 6], [7, 2], [10, 10, 10]]))
+
+if __name__ == "__main__":
+    main()
