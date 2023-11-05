13fc7e5ecc32a639684af7d29dbfcb8e38ca7f9d
heyhenry <henryyy3@hotmail.com>
2023-11-05 22:03:36 +0800

    pset_02 completed.

---
+++ November_2023/psets_revised/pset_01/second_vowel.py
@@ -0,0 +1,35 @@
+"""
+Returns the index of the second vowel in `s`
+If no vowels are in `s`, returns -1
+A vowel is defined to be 'aeiou', and a 'y' that _isn't_ the first character
+Examples:
+second_vowel('test') -> -1
+second_vowel('aeiou') -> 1
+second_vowel('slowly') -> 5
+second_vowel('yip') -> -1
+"""
+def second_vowel(s : str) -> int:
+
+    result = -1
+    counter = 0
+
+    for c in range(len(s)):
+        
+        if s[c] in 'aeiou' or s[c] == 'y' and c != 0:
+            counter += 1
+
+        if counter == 2:
+            result = c
+
+    return result 
+
+def main():
+    print(second_vowel('test'))
+    print(second_vowel('aeiou'))
+    print(second_vowel('slowly'))
+    print(second_vowel('yip'))
+    print(second_vowel('yppy'))
+    print(second_vowel('pygmy'))
+
+if __name__ == "__main__":
+    main()
---
+++ November_2023/psets_revised/pset_01/sum_all.py
@@ -0,0 +1,26 @@
+"""
+Returns the sum of each row of the table as a list
+Examples:
+sum_all([[1, 2, 3], [4, 5, 6]]) -> [6, 15]
+sum_all([[2], [4, 7], [-1, -2, -4, -5]]) -> [2, 11, -12]
+sum_all([[]]) -> []
+"""
+def sum_all(table : list[list[int]]) -> list[int]:
+
+    result = []
+
+    for row in table:
+        sum_val = 0
+        for i in row:
+            sum_val += i
+        result.append(sum_val)
+    
+    return result
+
+def main():
+    print(sum_all([[1, 2, 3], [4, 5, 6]]))
+    print(sum_all([[2], [4, 7], [-1, -2, -4, -5]]))
+    print(sum_all([[]]))
+
+if __name__ == "__main__":
+    main()
