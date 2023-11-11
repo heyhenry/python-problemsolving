ff4db0a1064e079da6cdb22e6bff2e756fa05eb5
heyhenry <henryyy3@hotmail.com>
2023-11-11 16:14:22 +0800

    Finally solved pset04q3. All test cases green.

--- November_2023/pset_04/whose_the_best.py
+++ November_2023/pset_04/whose_the_best.py
@@ -30,34 +30,56 @@ def whose_the_best(filename: str):
                     if i not in names and i != '<' and i != '>':
                         names.append(i)
 
-            # print(split_list)
-            
+            name_hierarchy = {}
 
+            for i in names:
+                name_hierarchy[i] = []
 
-            # name_hierarchy = {}
+            for row in content:
+                if row[1] == '<':
+                    name_hierarchy[row[2]].append(row[0])
+                else:
+                    name_hierarchy[row[0]].append(row[2])
 
-            # for i in names:
-            #     name_hierarchy[i] = []
-            
-            # for row in content:
-            #     if row[1] == '<':
-            #         name_hierarchy[row[2]].append(row[0])
-            #     elif row[1] == '>':
-            #         name_hierarchy[row[0]].append(row[2])
+            greatest_name = None
+            best_name = ''
 
             # print(name_hierarchy)
 
-        # return result
+            # to find best name if there is evidently only 1 based on greatest amount of names conquered
+            for keys, values in name_hierarchy.items():
+                if greatest_name is None:
+                    greatest_name = len(values)
+                    best_name = keys
+                elif len(values) > greatest_name:
+                    greatest_name = len(values)
+                    best_name = keys
 
+            # to find best name when it is more subtle by comparing against the current best name that has conquered the most names
+            for keys, values in name_hierarchy.items():
+                if best_name in values:
+                    best_name = keys
+
+            for keys, values in name_hierarchy.items():
+                if keys != best_name and greatest_name == len(values):
+                    if values == name_hierarchy[best_name]:
+                        best_name = 'Unknown'
+
+            # if there is a contradiction aka the best name that was found was in another key's values then it can be considered invalid
+            for keys, values in name_hierarchy.items():
+                if keys != best_name and best_name in values:
+                    best_name = 'Invalid'
+        
+        return best_name
 
 def main():
     print(whose_the_best('q3_data/input1.txt')) # Alice
-    # print(whose_the_best('q3_data/input2.txt')) # Invalid
-    # print(whose_the_best('q3_data/input3.txt')) # Unknown
-    # print(whose_the_best('q3_data/input4.txt')) # Alice
-    # print(whose_the_best('q3_data/input5.txt')) # Elmer
-    # print(whose_the_best('q3_data/input6.txt')) # Carol
-    # print(whose_the_best('q3_data/input7.txt')) # Invalid
+    print(whose_the_best('q3_data/input2.txt')) # Invalid
+    print(whose_the_best('q3_data/input3.txt')) # Unknown
+    print(whose_the_best('q3_data/input4.txt')) # Alice
+    print(whose_the_best('q3_data/input5.txt')) # Elmer
+    print(whose_the_best('q3_data/input6.txt')) # Carol
+    print(whose_the_best('q3_data/input7.txt')) # Invalid
 
 if __name__ == "__main__":
     main()
