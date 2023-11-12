6e82828bb3b15ec0489ee16535715336798d4764
heyhenry <henryyy3@hotmail.com>
2023-11-12 20:56:24 +0800

    Amended and solved q2 pset4.

--- November_2023/pset_04/image_flip.py
+++ November_2023/pset_04/image_flip.py
@@ -11,32 +11,22 @@ out images in the format we're using, so you can make a png or whatever of them!
 def flip_image(filename : str):
 
     with open(filename, 'r') as file:
+        
         read_content = file.read()
         
         if read_content:
+            
             split_content = read_content.splitlines()
             content = []
-            # for row in split_content:
-            #     content.append(row.split(' '))
+
             for row in split_content:
                 content.append(row.split(' '))
 
-            revised_content = []
-
-            # up and down flip 
-            for row in content:
-                row.reverse()
-                revised_content.append(row)
-
-            # # side to side flip
-            # for row in range(len(content)):
-            #     revised_content.append(content[row-1])
-
-            # print(revised_content)
+            content.reverse()
 
             with open('q2_data/test.output', 'w') as nfile:
                 
-                for row in revised_content:
+                for row in content:
                     for i in range(len(row)):
                         if i != len(row) - 1:
                             nfile.write(row[i])
@@ -47,17 +37,10 @@ def flip_image(filename : str):
                     
 def main():
     # print(flip_image("q2_data/input1.txt")) # passing
-    # print(flip_image("q2_data/input2.txt")) # passing
+    # print(flip_image("q2_data/input2.txt")) # does not pass 
     # print(flip_image("q2_data/input3.txt")) # passing
     # print(flip_image("q2_data/input4.txt")) # passing
-    print(flip_image("q2_data/input5.txt")) # wip
-
-# note to self: 
-# I have tried the side to side flip as I checked the first and last lines and replicated that, 
-# however the digits for the lines in between do not follow the same principle logic, 
-# this makes me think maybe there is more to it or it may be an error on the given output file for testcase 5. 
-# Either way, if I were to incorporate the side to side flip logic, it would make my first 4 test cases invalid. 
-# The current up and down (upside down) logic code seems to suffice imo in regards to the given guidelines but will investigate further.
+    print(flip_image("q2_data/input5.txt"))
 
 if __name__ == "__main__":
     main()
