6fd7da9339cead5f43766ccc7f42799b6ec891ed
heyhenry <henryyy3@hotmail.com>
2023-11-15 13:38:39 +0800

    solved twosum problem

---
+++ November_2023/NeetCode/two_sum.py
@@ -0,0 +1,45 @@
+"""
+1. Two Sum
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+"""
+def twoSum(nums : list[int], target : int) -> list[int]:
+
+    result = []
+
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                result.append(i)
+                result.append(j)
+
+    return result
+
+def main():
+    print(twoSum([2,7,11,15], 9)) # [0,1]
+    print(twoSum([3,2,4], 6)) # [1,2]
+    print(twoSum([3,3], 6)) # [0,1]
+
+if __name__ == "__main__":
+    main()
