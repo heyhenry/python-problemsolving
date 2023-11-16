907a6876132cd0aaeb175ac692e16e080cfb6543
heyhenry <henryyy3@hotmail.com>
2023-11-16 14:55:58 +0800

    Solved the problem, correct output is provided now.

--- November_2023/lang_creation/first_work/simple_lang.py
+++ November_2023/lang_creation/first_work/simple_lang.py
@@ -35,45 +35,50 @@ def simple_lang(filename : str):
             variables = []
             chr_pattern = re.compile(r"[A-Za-z]")
 
+            var_dict = {}
+
             # find existing values and assign its respective value or equation
             for row in content:
+                num1 = None
+                num2 = None
+                operand = None
+                var_val = 0
                 for i in range(len(row)):
                         if row[i] == 'int' and re.fullmatch(chr_pattern, row[i+1]):
                             variables.append(row[i+1])
+                            for j in range(i+3, len(row)):
+                                if num1 is None:
+                                    num1 = row[j]
+                                elif operand is None:
+                                    operand = row[j]
+                                elif num2 is None:
+                                    num2 = row[j]
+                            if num1.isdigit() == False:
+                                num1 = var_dict[num1]
+                            elif num2.isdigit() == False:
+                                num2 = var_dict[num2]
+                            
+                            if operand == '+':
+                                var_val = int(num1) + int(num2)
+                            elif operand == '-':
+                                var_val = int(num1) - int(num2)
+                            elif operand == '*':
+                                var_val = int(num1) * int(num2)
+                            elif operand == '/':
+                                var_val == int(num1) / int(num2)
+                            var_dict[row[i+1]] = var_val
+                            
                 del row[:3]
-            
-            equation_results = []
 
-            for row in content:
-                if row[0].isalpha():
-                    print('NOT A NUMBER')
-                elif row[2].isalpha():
-                    print('ALSO NOT A NUMBER')
-                elif row[1] == '-':
-                    equation_results.append(int(row[0]) - int(row[2]))
-                elif row[1] == '+':
-                    equation_results.append(int(row[0]) + int(row[2]))
-                elif row[1] == '*':
-                    equation_results.append(int(row[0]) * int(row[2]))
-                else:
-                    equation_results.append(int(row[0]) / int(row[2]))
-
-            # var_dict = {}
-
-            # for i in range(len(variables)):
-            #     var_dict[variables[i]] = equation_results[i]
-
-            # # prints out var_dict values
-            # for k, v in var_dict.items():
-            #     print(v)
-
-            # To Note for tomorrow: 
-            # might have to go back to dictionary creation inside the forloop where the del row[:3] is in
+            # prints out var_dict values
+            for k, v in var_dict.items():
+                print(v)
                 
 def main():
-    print(simple_lang('test_cases/input1.txt'))
-    # print(simple_lang('test_cases/input2.txt'))
-    # print(simple_lang('test_cases/input3.txt')) # 2, 4
+    print(simple_lang('test_cases/input1.txt')) # returns 2, 4
+    print(simple_lang('test_cases/input2.txt')) # returns 2
+    print(simple_lang('test_cases/input3.txt')) # returns 2, 4
+    print(simple_lang('test_cases/input4.txt')) # returns 2, 3
 
 if __name__ == '__main__':
     main()
