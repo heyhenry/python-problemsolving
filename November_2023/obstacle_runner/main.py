3a90d9d5b65b66d880aa68feb814ccf77c0e8e08
heyhenry <henryyy3@hotmail.com>
2023-11-24 18:10:44 +0800

    Added boundary checking for object.

--- November_2023/obstacle_runner/main.py
+++ November_2023/obstacle_runner/main.py
@@ -6,6 +6,9 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
+screen_width = screen.get_width()
+screen_height = screen.get_height()
+
 player_pos = pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
 
 while running:
@@ -18,6 +21,7 @@ while running:
 
     pygame.draw.circle(screen, 'purple', player_pos, 40)
 
+    # player movement
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         player_pos.y -= 300 * dt
@@ -27,6 +31,16 @@ while running:
         player_pos.x -= 300 * dt
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
+    
+    # resources used for boundary code below (appendix 1)
+    if player_pos.x >= screen_width-40:
+        player_pos.x -= 300 * dt
+    if player_pos.x <= 0+40:
+        player_pos.x += 300 * dt
+    if player_pos.y >= screen_height-40:
+        player_pos.y -= 300 * dt
+    if player_pos.y <= 0+40:
+        player_pos.y += 300 * dt
 
     pygame.display.flip()
 
