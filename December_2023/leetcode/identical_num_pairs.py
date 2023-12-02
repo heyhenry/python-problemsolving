2014d5dc521505c480c1c5b4c831e44a65d496b1
heyhenry <henryyy3@hotmail.com>
2023-12-03 04:49:01 +0800

    solved identical_num_pairs

---
+++ December_2023/leetcode/identical_num_pairs.py
@@ -0,0 +1,48 @@
+"""
+1512. Number of Good Pairs
+
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+Example 2:
+
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+"""
+def num_identical_pairs(nums : list[int]) -> int:
+
+    pairs = []
+
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] == nums[j] and i < j:
+                pairs.append('(' + str(i) + ',' + str(j) + ')')
+
+    return pairs
+
+def main():
+    print(num_identical_pairs([1,2,3,1,1,3]))
+    print(num_identical_pairs([1,1,1,1]))
+    print(num_identical_pairs([1,2,3]))
+
+if __name__ == "__main__":
+    main()
