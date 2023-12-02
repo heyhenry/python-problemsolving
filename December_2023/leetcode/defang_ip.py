f2560be48467546bc29690777cf0f2c2de57519a
heyhenry <henryyy3@hotmail.com>
2023-12-03 04:29:13 +0800

    solved defang_ip

---
+++ December_2023/leetcode/defang_ip.py
@@ -0,0 +1,43 @@
+"""
+1108. Defanging an IP Address
+
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+
+ 
+
+Example 1:
+
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+Example 2:
+
+Input: address = "255.100.50.0"
+Output: "255[.]100[.]50[.]0"
+ 
+
+Constraints:
+
+The given address is a valid IPv4 address.
+"""
+def defang_ip(address: str) -> str:
+
+    defanged_address = ''
+
+    for c in range(len(address)):
+        if address[c] == '.':
+            defanged_address += '['
+            defanged_address += '.'
+            defanged_address += ']'
+        else:
+            defanged_address += address[c]
+
+    return defanged_address
+
+def main():
+    print(defang_ip("1.1.1.1"))
+    print(defang_ip("255.100.50.0"))
+
+if __name__ == "__main__":
+    main()
