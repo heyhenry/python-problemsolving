456ae44c6cf92b8687dcfe3ea0d63da69cb98ed7
heyhenry <henryyy3@hotmail.com>
2023-12-08 10:43:09 +0800

    Created random number guessing game.

---
+++ December_2023/fudge_around/guess_the_number.py
@@ -0,0 +1,16 @@
+import random
+
+random_num = random.randint(0, 10)
+not_num = True
+attempts = 0
+while not_num:
+    user_input = int(input("Guess a number between 0 - 10: "))
+    attempts += 1
+    if user_input == random_num:
+        print("You guessed the number!")
+        not_num = False
+    print(random_num)
+    
+print("The number was " + str(random_num))
+print("It took you " + str(attempts) + " attempts to guess the correct number.")
+
