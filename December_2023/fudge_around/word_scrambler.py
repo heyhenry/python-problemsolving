c9c510650eba16d680c482608af07dc112119a76
heyhenry <henryyy3@hotmail.com>
2023-12-08 11:15:58 +0800

    Added list generation and validation checks.

--- December_2023/fudge_around/word_scrambler.py
+++ December_2023/fudge_around/word_scrambler.py
@@ -4,35 +4,72 @@ user_input = input("Enter words to be scrambled into a single 'word': ")
 
 not_satisfied = True
 user_choice = 0
-counter = 0
-
+generations = 0
+selected_word = ''
 while not_satisfied:
-    
-    counter += 1
-    temp_words = [w for w in user_input if w != ' ']
-    random.shuffle(temp_words)
-    word = ''
-    for w in temp_words:
-        word += w
 
-    print("\nNew generated word: " + word)
-    print("====")
-    print("1. Generate a new one")
-    print("2. I like the word")
-    user_choice = int(input("Are you satisfied with the generated word? "))
+    print("\nMenu")
+    print("----")
+    print("1. Generate 1 new word")
+    print("2. Generate 5 new words")
+    print("3. Exit")
+    user_choice = input("Select an option between 1-3: ")
+    generated_list = []
+    if user_choice.isdigit():
+        if int(user_choice) == 1:
+            generations += 1
+            temp_words = [w for w in user_input if w != ' ']
+            random.shuffle(temp_words)
+            word = ''
+            for w in temp_words:
+                word += w
+            print("1 | " + word)
+            print("\n1. Select generated word")
+            print("2. Try again")
+            user_list_choice = input("Do you wish to select this word: ")
 
-    if user_choice != 1 and user_choice != 2:
-        print("Incorrect choice. We will proceed to generate a new word for you.")
-    elif user_choice == 2:
-        print("Thank you for using our service.")
-        not_satisfied = False
-    else: 
-        continue
+            if user_list_choice == '1':
+                selected_word = word
+                not_satisfied = False
+            elif user_list_choice == '2':
+                print("Going back to main menu...")
+            else:
+                print("Invalid user input. Going back to the main menu..")
+                 
+        elif int(user_choice) == 2:
+            generations += 5
+            i = 0
+            while i < 5:
+                i += 1
+                temp_words = [w for w in user_input if w != ' ']
+                random.shuffle(temp_words)
+                word = ''
+                for w in temp_words:
+                    word += w
+                generated_list.append(word)
+            for word in range(len(generated_list)):
+                print(str(word + 1) + "|" + generated_list[word])
+            print('6 | Return to Menu')
+            user_list_choice = int(input("Selected desired generated word via their number: "))
+            if user_list_choice > 0 and user_list_choice < 11:
+                selected_word = generated_list[user_list_choice - 1]
+                not_satisfied = False
+    else:
+        print("Invalid user input. Returning to menu..")    
 
-print("Summary Details: ")
+
+    # if user_choice != 1 and user_choice != 2:
+    #     print("Incorrect choice. We will proceed to generate a new word for you.")
+    # elif user_choice == 2:
+    #     print("Thank you for using our service.")
+    #     not_satisfied = False
+    # else: 
+    #     continue
+
+print("\nSummary Details: ")
 print("-------")
-print("Generated word chosen: " + word) 
-print("Generated a total of " + str(counter) + " times.")
+print("Generated word chosen: " + selected_word) 
+print("Generated a total of " + str(generations) + " times.")
     
 
 # things i can implement 
