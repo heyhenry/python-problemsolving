a8f3e431d42cc7c264dcf5de599774db12772c03
heyhenry <henryyy3@hotmail.com>
2023-12-08 11:30:21 +0800

    Updated code to provide smooth interchangeable flow between different menu options. Advanced options for generation added too.

--- December_2023/fudge_around/word_scrambler.py
+++ December_2023/fudge_around/word_scrambler.py
@@ -6,6 +6,7 @@ not_satisfied = True
 user_choice = 0
 generations = 0
 selected_word = ''
+
 while not_satisfied:
 
     print("\nMenu")
@@ -14,30 +15,41 @@ while not_satisfied:
     print("2. Generate 5 new words")
     print("3. Exit\n")
     user_choice = input("Select an option between 1-3: ")
+    
     generated_list = []
+    
     if user_choice.isdigit():
+        
         if int(user_choice) == 1:
-            generations += 1
-            temp_words = [w for w in user_input if w != ' ']
-            random.shuffle(temp_words)
-            word = ''
-            for w in temp_words:
-                word += w
-            
-            print("1 | " + word)
-            print("\n1. Select generated word")
-            print("2. Try again")
-            user_list_choice = input("\nDo you wish to select this word: ")
+            is_single_generation = True
+            while is_single_generation:
+                generations += 1
+                temp_words = [w for w in user_input if w != ' ']
+                random.shuffle(temp_words)
+                word = ''
+                for w in temp_words:
+                    word += w
+                
+                print("1 | " + word)
+                print("\n1. Select generated word")
+                print("2. Try again")
+                print("3. Go to menu")
+                user_list_choice = input("\nDo you wish to select this word: ")
 
-            if user_list_choice.isdigit():
-                if int(user_list_choice) == 1:
-                    selected_word = word
-                    not_satisfied = False
-                elif int(user_list_choice) == 2:
-                    print("Going back to main menu...")
-                else:
-                    print("Invalid user input. Going back to the main menu..")
-                 
+                if user_list_choice.isdigit():
+                    if int(user_list_choice) == 1:
+                        selected_word = word
+                        not_satisfied = False
+                        is_single_generation = False
+                    elif int(user_list_choice) == 2:
+                        print("\nRunning it back..")
+                    elif int(user_list_choice) == 3:
+                        print("\nReturning to menu..")
+                        is_single_generation = False
+                    else:
+                        print("\nInvalid user input. Going back to the main menu..")
+                        is_single_generation = False
+                    
         elif int(user_choice) == 2:
 
             generations += 5
@@ -68,18 +80,10 @@ while not_satisfied:
         print("Invalid user input. Returning to menu..")    
 
 
-    # if user_choice != 1 and user_choice != 2:
-    #     print("Incorrect choice. We will proceed to generate a new word for you.")
-    # elif user_choice == 2:
-    #     print("Thank you for using our service.")
-    #     not_satisfied = False
-    # else: 
-    #     continue
-
 print("\nSummary Details: ")
 print("-------")
 print("Generated word chosen: " + selected_word) 
-print("Generated a total of " + str(generations) + " times.")
+print("Generated a total of " + str(generations) + " times.\n")
     
 
 # things i can implement 
