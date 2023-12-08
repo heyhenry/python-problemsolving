7865b27005983a7357bb1364628961d0912520a1
heyhenry <henryyy3@hotmail.com>
2023-12-08 11:22:25 +0800

    Tidied up validation and syntax.

--- December_2023/fudge_around/word_scrambler.py
+++ December_2023/fudge_around/word_scrambler.py
@@ -12,7 +12,7 @@ while not_satisfied:
     print("----")
     print("1. Generate 1 new word")
     print("2. Generate 5 new words")
-    print("3. Exit")
+    print("3. Exit\n")
     user_choice = input("Select an option between 1-3: ")
     generated_list = []
     if user_choice.isdigit():
@@ -23,22 +23,26 @@ while not_satisfied:
             word = ''
             for w in temp_words:
                 word += w
+            
             print("1 | " + word)
             print("\n1. Select generated word")
             print("2. Try again")
-            user_list_choice = input("Do you wish to select this word: ")
+            user_list_choice = input("\nDo you wish to select this word: ")
 
-            if user_list_choice == '1':
-                selected_word = word
-                not_satisfied = False
-            elif user_list_choice == '2':
-                print("Going back to main menu...")
-            else:
-                print("Invalid user input. Going back to the main menu..")
+            if user_list_choice.isdigit():
+                if int(user_list_choice) == 1:
+                    selected_word = word
+                    not_satisfied = False
+                elif int(user_list_choice) == 2:
+                    print("Going back to main menu...")
+                else:
+                    print("Invalid user input. Going back to the main menu..")
                  
         elif int(user_choice) == 2:
+
             generations += 5
             i = 0
+            
             while i < 5:
                 i += 1
                 temp_words = [w for w in user_input if w != ' ']
@@ -47,13 +51,19 @@ while not_satisfied:
                 for w in temp_words:
                     word += w
                 generated_list.append(word)
+            
             for word in range(len(generated_list)):
-                print(str(word + 1) + "|" + generated_list[word])
-            print('6 | Return to Menu')
-            user_list_choice = int(input("Selected desired generated word via their number: "))
-            if user_list_choice > 0 and user_list_choice < 11:
-                selected_word = generated_list[user_list_choice - 1]
-                not_satisfied = False
+                print(str(word + 1) + "| " + generated_list[word])
+            print('6| Return to Menu')
+            
+            user_list_choice = input("\nSelected desired generated word via their number: ")
+            
+            if user_list_choice.isdigit():
+                if int(user_list_choice) > 0 and int(user_list_choice) < 6:
+                    selected_word = generated_list[int(user_list_choice) - 1]
+                    not_satisfied = False
+                elif int(user_list_choice) == 6:
+                    continue
     else:
         print("Invalid user input. Returning to menu..")    
 
