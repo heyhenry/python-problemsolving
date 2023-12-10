ccc96cab503cb7409e218739bf3a9dd43da173f4
heyhenry <henryyy3@hotmail.com>
2023-12-10 11:44:50 +0800

    alternative solution for min_four

--- December_2023/psets/pset_01/min_four.py
+++ December_2023/psets/pset_01/min_four.py
@@ -5,9 +5,22 @@ min_four(1, 2, 3, 4) -> 1
 min_four(3, 2, 4, 6) -> 2
 min_four(0, 0, -5, 0) -> -5
 """
+# solution 1 - Using the min function
+# def min_four(x : int, y : int, z : int, w : int) -> int:
+
+#     return min(x, y, z, w)
+
+# solution 2 - Using the max function
 def min_four(x : int, y : int, z : int, w : int) -> int:
 
-    return min(x, y, z, w)
+    nums = [x, y, z, w]
+
+    while len(nums) > 1:
+
+        mnum = max(nums)
+        nums.remove(mnum)
+
+    return nums[0]
 
 def main():
     print(min_four(1, 2, 3, 4))
