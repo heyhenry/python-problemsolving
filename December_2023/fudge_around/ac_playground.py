8d51750b1067f74602a196e970a4280381ae6418
heyhenry <henryyy3@hotmail.com>
2023-12-12 08:20:24 +0800

    Added helper function for first player decision.

--- December_2023/fudge_around/ac_playground.py
+++ December_2023/fudge_around/ac_playground.py
@@ -1,5 +1,7 @@
 import time
+import sys
 
+# version 1 of the loading simulator
 # print("Loading.....")
 # time.sleep(0.5)
 # print("Loading....")
@@ -10,9 +12,25 @@ import time
 # time.sleep(0.5)
 # print("Loaded!")
 
-loading = ["Loading... 0%", "Loading... 5%", "Loading... 10%", "Loading... 30%", "Loading... 50%", "Loading... 70%", "Loading... 100%", "Loaded!"]
+# version 2 of the loading simulator
+# loading = ["Loading... 0%", "Loading... 5%", "Loading... 10%", "Loading... 30%", "Loading... 50%", "Loading... 70%", "Loading... 100%", "Loaded!"]
 
-for i in loading:
-    print(i)
-    time.sleep(0.8)
+# for i in loading:
+#     print(i)
+#     time.sleep(0.8)
 
+# testing gender choice selection
+# gender = input("Select Gender: \n1. Male \n2. Female \n:")
+
+# if gender == '1' or gender.lower() == 'male':
+#     print("You're a male")
+# elif gender == '2' or gender.lower() == 'female':
+#     print("You're a female")
+# else:
+#     sys.exit(0)
+
+# testing icon display params
+player_icon = ''
+print(player_icon)
+player_icon = '😼'
+print(player_icon)
--- December_2023/fudge_around/aragon_chronicles.py
+++ December_2023/fudge_around/aragon_chronicles.py
@@ -3,20 +3,58 @@ The first installment of a text based adventure game with choices for the Aragon
 """
 import time
 
-# variables
-username = ''
-hearts = 5
-mana = 1
-inventory = ['guiding stick']
+# helper functions
 
-# the game 
-username = input("Enter your adventurer's name: ")
+# provide result from 2 given choices, else continue to repeat question
+def make_choice(choice_one : str, choice_two: str) -> int:
 
-loading = ["Loading... 0%", "Loading... 5%", "Loading... 10%", "Loading... 30%", "Loading... 50%", "Loading... 70%", "Loading... 100%", "Loaded!"]
+    valid_choice = True
+    choice = ''
+    while valid_choice:
 
-for i in loading:
-    print(i)
-    time.sleep(0.8)
+        print('[1]:' + choice_one)
+        print('[2]:' + choice_two)
+        choice = input("Select a Choice: ")
 
-print("[ Welcome To Aragon Chronicles! ]")
+        if choice == '1' or choice == '2':
+            valid_choice = False
+        else:
+            print("\nInvalid choice. Please enter number 1 or 2.")
 
+    return choice
+
+def main():
+
+    # variables
+    username = ''
+    hearts = 5
+    mana = 1
+    inventory = ['guiding stick']
+
+    # the game 
+
+    # loading = ["Loading... 0%", "Loading... 5%", "Loading... 10%", "Loading... 30%", "Loading... 50%", "Loading... 70%", "Loading... 100%", "Loaded!"]
+
+    # for i in loading:
+    #     print(i)
+    #     time.sleep(0.8)
+
+    print("[ Welcome To Aragon Chronicles! ]")
+    username = input("Enter your adventurer's name: ")
+    
+    print("You wake up in an unfamiliar surrounding. \nThe environment is cold and covered with an unknown purple mist.")
+    print("There is something glowing next to you, but you are unable to see it clearly.")
+    
+    choice = make_choice("You decide to investigate what the glowing thing is.", "You decide to ignore it and begin wandering around.")
+    if choice == '1':
+        print("One Here")
+    elif choice == '2':
+        print("Two Here")
+
+    print("End of cycle")
+
+if __name__ == "__main__":
+    main()
+
+
+# look into the make_choice function and its counter part call in the main function. There is potentially some redudancy that can be reduced.
