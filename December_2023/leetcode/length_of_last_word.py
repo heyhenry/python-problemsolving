853a55c26e497a37d796fabe4efc1567988dc1e4
heyhenry <henryyy3@hotmail.com>
2023-12-13 11:26:49 +0800

    provided a more succint solution for previous leetcode problem

--- December_2023/leetcode/length_of_last_word.py
+++ December_2023/leetcode/length_of_last_word.py
@@ -32,20 +32,42 @@ Constraints:
 s consists of only English letters and spaces ' '.
 There will be at least one word in s.
 """
+# # solution 1
+# def length_of_last_word(s : str) -> int:
+
+    # # removes spaces between words
+    # temp = s.split(' ')
+
+    # result = []
+
+    # for i in temp:
+    #     if i != '':
+    #         # only add elements that are not an empty string
+    #         result.append(i)
+
+    # # return the last lenghth of the last element in the result list
+    # return len(result[len(result)-1])
+
 def length_of_last_word(s : str) -> int:
-
-    # solution 1
-    # removes spaces between words?
-    temp = s.split(' ')
-
     result = []
-
-    for i in temp:
-        if i != '':
-            # only add elements that are not an empty string
-            result.append(i)
-
-    # return the last lenght of the last element in the result list
+    word = ''
+    
+    for c in range(len(s)):
+        # checks to see if the next char is a letter or whitespace/space
+        if s[c].isalpha():
+            # if valid, will concatenate the chars to form words
+            word += s[c]
+        elif not s[c].isalpha():
+            # checks length of word to ensure it isnt just whitespace/space
+            if len(word) > 0:
+                # adds the verified word to the list of results
+                result.append(word)
+            word = ''
+        # checks if the last character in the string is a whitespace, if not add the final word
+        if c == len(s)-1 and s[c].isalpha():
+            result.append(word)
+    
+    # return the last length of the last element in the result list
     return len(result[len(result)-1])
 
 def main():
