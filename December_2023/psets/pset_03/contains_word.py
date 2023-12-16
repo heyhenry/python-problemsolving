93b7fd82425b5304a7828f5ff147f406a91de980
heyhenry <henryyy3@hotmail.com>
2023-12-16 10:33:10 +0800

    solved contains_word

---
+++ December_2023/psets/pset_03/contains_word.py
@@ -0,0 +1,38 @@
+"""
+Returns whether the given word appears in a `.fakeletters` file
+Requires that `filename` ends with `.fakeletters`
+Examples:
+contains("test1.fakeletters", "b") -> True
+contains("test1.fakeletters", "d") -> False
+contains("test2.fakeletters", "wor") -> False
+contains("test2.fakeletters", "hello") -> True
+contains("test3.fakeletters", "") -> False
+contains("test4.fakeletters") -> 69 (nice!)
+"""
+def contains(filename : str, word : str) -> bool:
+    
+    result = False
+    with open(filename, 'r') as file:
+
+        read_content = file.read()
+
+        if read_content:
+
+            store_lines = read_content.splitlines()
+
+            for row in store_lines:
+                for i in row.split(' '):
+                    if i == word:
+                        result = True
+
+    return result
+
+def main():
+    print(contains("fakeletters/test1.fakeletters", "b"))
+    print(contains("fakeletters/test1.fakeletters", "d"))
+    print(contains("fakeletters/test2.fakeletters", "wor"))
+    print(contains("fakeletters/test2.fakeletters", "hello"))
+    print(contains("fakeletters/test3.fakeletters", ""))
+
+if __name__ == "__main__":
+    main()
