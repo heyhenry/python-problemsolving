4269bce9a9fcc048c1615b31a4305812078649e2
heyhenry <henryyy3@hotmail.com>
2023-12-29 03:05:12 +0800

    solved contains

--- December_2023/psets/psets_run_it_back/pset_03/contains.py
+++ December_2023/psets/psets_run_it_back/pset_03/contains.py
@@ -11,4 +11,28 @@ contains("test4.fakeletters") -> 69 (nice!)
 """
 def contains(filename : str, word : str) -> bool:
 
-    
+    result = False
+
+    with open(filename, 'r') as file:
+
+        read_content = file.read()
+
+        if read_content:
+
+            content = read_content.split(' ')
+
+            for i in content:
+                if i == word:
+                    result = True
+
+    return result
+
+def main():
+    print(contains('fakeletters/test1.fakeletters','b'))
+    print(contains('fakeletters/test1.fakeletters','d'))
+    print(contains('fakeletters/test2.fakeletters','wor'))
+    print(contains('fakeletters/test2.fakeletters','hello'))
+    print(contains('fakeletters/test3.fakeletters',''))
+
+if __name__ == "__main__":
+    main()
