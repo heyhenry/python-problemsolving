e16113f96a7af1b37d6c54189ce57f51d35636b7
heyhenry <henryyy3@hotmail.com>
2023-12-29 03:18:17 +0800

    solved row_sum

---
+++ December_2023/psets/psets_run_it_back/pset_03/transpose.py
@@ -0,0 +1,21 @@
+def transpose(filename : str):
+"""
+Given a CSV filename `example.csv`
+4 / 5
+If that file is in the form of a matrix (each row has the same length)
+Writes the transpose of that file to `example_transpose.csv`
+Otherwise, this function does nothing
+A matrix transpose "flips" the matrix over the diagonal:
+https://en.wikipedia.org/wiki/Transpose
+Examples:
+transpose("data1.csv") produces an empty file `data1_transpose.csv`
+transpose("data2.csv") does nothing
+transpose("data3.csv") produces `data3_transpose.csv` with
+1,3,5
+-1,-2,6
+-2,-5,7
+transpose("data4.csv") produces `data4_transpose.csv` with
+1,2,3,4
+2,3,4,5
+3,4,5,6
+"""
