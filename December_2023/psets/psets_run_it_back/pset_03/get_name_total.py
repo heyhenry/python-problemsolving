1ee9a9a9ba1276ef4a443dec2b9cfa6bf0eb990e
heyhenry <henryyy3@hotmail.com>
2023-12-29 05:04:27 +0800

    solved get_name_total

--- December_2023/psets/psets_run_it_back/pset_03/get_name_total.py
+++ December_2023/psets/psets_run_it_back/pset_03/get_name_total.py
@@ -1,4 +1,3 @@
-def get_name_total(filename : str, name : str) -> int:
 """
 Given a filename ending with `csv`
 Returns the total associated with `name`
@@ -6,10 +5,78 @@ If the table has no `total` column or `name` is not found, returns -1
 If the file contains `first name` and `last name` rather than `name`
 Then this function should treat those as concatenated by a space
 Examples:
-contains_name("data1.csv", "goblin") -> -1 // no total
-contains_name("data2.csv", "bob smith") -> 1234
-contains_name("data2.csv", "alice jones") -> 6712
-contains_name("data3.csv", "Calvin Harris") -> 64953382
-contains_name("data3.csv", "The Weeknd") -> 108390904
-contains_name("data3.csv", "Test") -> -1
+get_name_total("tables/table1.csv", "goblin") -> -1 // no total
+get_name_total("tables/table2.csv", "bob smith") -> 1234
+get_name_total("tables/table2.csv", "alice jones") -> 6712
+get_name_total("tables/table3.csv", "Calvin Harris") -> 64953382
+get_name_total("tables/table3.csv", "The Weeknd") -> 108390904
+get_name_total("tables/table3.csv", "Test") -> -1
 """
+# Note: used thes same algorithm/logic as contains_name problem
+def get_name_total(filename : str, name : str) -> int:
+
+    result = -1
+
+    with open(filename, 'r') as file:
+
+        read_content = file.read()
+
+        if read_content:
+
+            split_lines = read_content.splitlines()
+            content = []
+
+            for row in split_lines:
+                content.append(row.split(','))
+
+            name_found = False
+            name_index = -1
+
+            firstname_found = False
+            lastname_found = False
+            firstname_index = -1
+            lastname_index = -1
+
+            total_found = False
+            total_index = -1
+
+            for column in range(len(content[0])):
+                if content[0][column] == 'name':
+                    name_found = True
+                    name_index = column
+                elif content[0][column] == 'first name':
+                    firstname_found = True
+                    firstname_index = column
+                elif content[0][column] == 'last name':
+                    lastname_found = True
+                    lastname_index = column
+                elif content[0][column] == 'total':
+                    total_found = True
+                    total_index = column
+
+            if name_found and total_found:
+
+                for row in content:
+                    if row[name_index] == name:
+                        result = row[total_index]
+
+            elif firstname_found and lastname_found and total_found:
+
+                fullname = name.split(' ')
+
+                for row in content:
+                    if row[firstname_index] == fullname[0] and row[lastname_index] == fullname[1]:
+                        result = row[total_index]
+
+    return result
+
+def main():
+    print(get_name_total("tables/table1.csv", "goblin"))
+    print(get_name_total("tables/table2.csv", "bob smith"))
+    print(get_name_total("tables/table2.csv", "alice jones"))
+    print(get_name_total("tables/table3.csv", "Calvin Harris"))
+    print(get_name_total("tables/table3.csv", "The Weeknd"))
+    print(get_name_total("tables/table3.csv", "Test"))
+
+if __name__ == "__main__":
+    main()
