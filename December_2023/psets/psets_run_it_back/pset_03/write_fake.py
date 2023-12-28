8ede55be7b917c5fff3b37de098631e43cba0831
heyhenry <henryyy3@hotmail.com>
2023-12-29 03:09:19 +0800

    solved write_fake

--- December_2023/psets/psets_run_it_back/pset_03/write_fake.py
+++ December_2023/psets/psets_run_it_back/pset_03/write_fake.py
@@ -10,4 +10,26 @@ contains("output.fakeletters", "test") -> True
 contains("output.fakeletters", "other") -> False
 count_words("output.fakeletters") -> 2
 """
+from contains import contains
+from count_words import count_words
+
 def write_fake(filename : str, words : list[str]):
+
+    with open(filename, 'w') as file:
+
+        for i in range(len(words)):
+            if i != len(words)-1:
+                file.write(words[i] + ' ')
+            else:
+                file.write(words[i])
+
+def main():
+    
+    write_fake("fakeletters/output.fakeletters", ["test", "out"])
+
+    print(contains("fakeletters/output.fakeletters", "test")) 
+    print(contains("fakeletters/output.fakeletters", "other")) 
+    print(count_words("fakeletters/output.fakeletters"))
+
+if __name__ == "__main__":
+    main()
