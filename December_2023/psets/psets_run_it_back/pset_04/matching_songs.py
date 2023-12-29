74bb8e901a5c138c8e2c17a611d0c06147420e29
heyhenry <henryyy3@hotmail.com>
2023-12-29 22:36:00 +0800

    comment justifying code integrity.

--- December_2023/psets/psets_run_it_back/pset_04/matching_songs.py
+++ December_2023/psets/psets_run_it_back/pset_04/matching_songs.py
@@ -54,3 +54,13 @@ if __name__ == "__main__":
 # it seems the solution I have given here actually makes more sense. 
 # It is late right now but I will review why my previous iteration worked and why the given output solutions were as they were
 # as they seem incorrect to me now. 
+    
+# UPDATE NOTE:
+# After reviewing the previous iteration code, problem description. I am confident that my current solution is correct for the given coding problem.
+# I believe the issue in regards to why my previous iteration of this problem was able to produce the given solution output is due to overlooking the index counter.
+# I believe it does not reset in the case of the character matched against the filter is the same as the previous character.
+
+# Additional analysis using chatgpt noted the following transcript:
+# There is a potential issue in the code that might lead to incorrect results. 
+# The code compares each character in the song against the corresponding character in the filter, and it increments the index only when a match is found. 
+# If there is a character in the song that matches the first character of the filter but does not match the subsequent characters, the index might not be reset, leading to incorrect matches.    
