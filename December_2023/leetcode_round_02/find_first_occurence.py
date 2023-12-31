3b5f2c42c48ffbbda9d8a7f203bedcca7976f0bb
heyhenry <henryyy3@hotmail.com>
2023-12-31 11:07:16 +0800

    solved find_first_occurence

---
+++ December_2023/leetcode_round_02/find_first_occurence.py
@@ -0,0 +1,41 @@
+"""
+28. Find the Index of the First Occurrence in a String
+
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ 
+
+Constraints:
+
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+"""
+def str_str(haystack: str, needle: str) -> int:
+
+    result = -1
+
+    for i in range(len(haystack)):
+        if haystack[i] == needle[0]:
+            if haystack[i:len(needle)] == needle:
+                result = i
+
+    return result
+            
+def main():
+    print(str_str(haystack = "sadbutsad", needle = "sad"))
+    print(str_str(haystack = "leetcode", needle = "leeto"))
+
+if __name__ == "__main__":
+    main()
