2446352ab165d91df2e8eb3e10c9dad41ba003e5
heyhenry <henryyy3@hotmail.com>
2024-01-05 17:57:26 +0800

    solved find words containing character

--- 2024/January/leetcode/2824.py
+++ 2024/January/leetcode/2824.py
@@ -1,9 +1,6 @@
 """
 2824. Count Pairs Whose Sum is Less than Target
-Easy
-409
-31
-Companies
+
 Given a 0-indexed integer array nums of length n and an integer target, return the number of pairs (i, j) where 0 <= i < j < n and nums[i] + nums[j] < target.
  
 
