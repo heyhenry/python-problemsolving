fcbfefbe1e2a85c2ba8000ed03f78840237f9584
heyhenry <henryyy3@hotmail.com>
2024-01-09 19:33:50 +0800

    wip - leetcode no.3 runitback

---
+++ 2024/January/leetcode/3-1.py
@@ -0,0 +1,55 @@
+"""
+3. Longest Substring Without Repeating Characters
+
+Given a string s, find the length of the longest 
+substring
+ without repeating characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+"""
+def lengthOfLongestSubstring(s: str) -> int:
+
+    distinct_letters = set(s)
+    size = len(s)
+    left, right = 0, 0
+
+    for i in range(size):
+        if s[right] in distinct_letters:
+            
+    
+
+
+
+    # return subs
+
+
+def main():
+    print(lengthOfLongestSubstring("abcabcbb"))
+    print(lengthOfLongestSubstring("bbbbb"))
+    print(lengthOfLongestSubstring("pwwkew"))
+
+if __name__ == "__main__":
+    main()
