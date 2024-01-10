34bb8bfe5354b8597926b53020d0ebe0785523d9
heyhenry <henryyy3@hotmail.com>
2024-01-10 20:30:40 +0800

    seemed to be solved 90%+ success but timelimit exceed issue.

---
+++ 2024/January/leetcode/15.py
@@ -0,0 +1,72 @@
+"""
+15. 3Sum
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+ 
+
+Constraints:
+
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+"""
+def threeSum(nums: list[int]) -> list[list[int]]:
+
+    n = len(nums)
+
+    results = []
+
+    for i in range(n):
+        for j in range(i+1, n):
+            for k in range(j+1, n):
+                temp = []
+                if i != j and i != k and j != k and nums[i] + nums[j] + nums[k] == 0:
+                    temp.extend([nums[i], nums[j], nums[k]])
+                    if temp not in results:
+                        results.append(temp)
+
+    new_results = []
+    for row in results:
+        new_results.append(sorted(row))
+
+    final_results = []
+
+    for i in new_results:
+        if i not in final_results:
+            final_results.append(i)
+
+    return final_results      
+
+def main():
+    # print(threeSum(nums = [-1,0,1,2,-1,-4]))
+    # print(threeSum(nums = [0,1,1]))
+    # print(threeSum(nums = [0,0,0]))
+    # print(threeSum([-1,0,1,2,-1,-4,-2,-3,3,0,4]))
+    print(threeSum([-1,0,1,2,-1,-4]))
+
+if __name__ == "__main__":
+    main()
