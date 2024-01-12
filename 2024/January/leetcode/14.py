9d3b1d52a688bd84ddf9a08c909b9cb804e8c76d
heyhenry <henryyy3@hotmail.com>
2024-01-12 20:28:03 +0800

    working on my solution for lc14

--- 2024/January/leetcode/14.py
+++ 2024/January/leetcode/14.py
@@ -27,15 +27,45 @@ strs[i] consists of only lowercase English letters.
 def longestCommonPrefix(strs: list[str]) -> str:
 
     # solution by https://leetcode.com/problems/longest-common-prefix/solutions/3273176/python3-c-java-19-ms-beats-99-91/
-    ans = ''
-    strs = sorted(strs)
-    first = strs[0]
-    last = strs[-1]
-    for i in range(min(len(first),len(last))):
-        if(first[i]!=last[i]):
-            return ans
-        ans+=first[i]
-    return ans
+    # ans = ''
+    # strs = sorted(strs)
+    # first = strs[0]
+    # last = strs[-1]
+    # for i in range(min(len(first),len(last))):
+    #     if(first[i]!=last[i]):
+    #         return ans
+    #     ans+=first[i]
+    # return ans
+
+    result = ''
+
+    if len(strs) == 1:
+        result = strs[0]
+        
+    else:
+        min_len = min(strs, key=len)
+        
+        prefixes = []
+        
+        prefix = ''
+        for col in range(len(min_len)):
+            temp = ''
+            for row in range(len(strs)):
+                temp += strs[row][col]
+            if temp == temp[0]*len(temp) and len(temp) > 1 and col == len(min_len) - 1:
+                prefix += temp[0]
+                prefixes.append(prefix)
+            elif temp == temp[0]*len(temp) and len(temp) > 1:
+                prefix += temp[0]
+            else:
+                prefixes.append(prefix)
+                prefix = ''
+        if len(prefixes) < 1:
+            result = strs[0]
+        else:
+            result = max(prefixes, key=len)
+        
+    return result
 
 def main():
     print(longestCommonPrefix(strs = ["flower","flow","flight"]))
