2a9ff1c3ed5df1ccd1d720cf5263897b25a17945
heyhenry <henryyy3@hotmail.com>
2024-02-05 07:59:28 +0800

    solved q3 pset4

--- 2024/February/psets/pset_04/whose_best.py
+++ 2024/February/psets/pset_04/whose_best.py
@@ -1,5 +1,7 @@
 def whose_best(filename : str) -> str:
 
+    result = ''
+
     with open(filename, 'r') as file:
 
         read_content = file.read()
@@ -26,21 +28,41 @@ def whose_best(filename : str) -> str:
                     else:
                         name_hierarchy[row[0]].append(row[2])
 
-            potential_besties = []
-
+            # add all inherited key names to keys
             for key, val in name_hierarchy.items():
                 for sec_key, sec_val in name_hierarchy.items():
-                     
+                    if key in sec_val:
+                        for v in val:
+                            if v not in name_hierarchy[sec_key]:
+                                name_hierarchy[sec_key].append(v)
+
+            potential_besties = []
             
+            for key, val in name_hierarchy.items():
+                name_in_val_counter = 0
+                for sec_key, sec_val in name_hierarchy.items():
+                     if key in sec_val:
+                         name_in_val_counter += 1
+                if name_in_val_counter < 1:
+                    potential_besties.append(key)
+            
+            if len(potential_besties) == 1:
+                result = potential_besties[0]
+            elif len(potential_besties) < 1:
+                result = 'Invalid'
+            else:
+                result = 'Unknown'
+
+    return result 
 
 def main():
-    # print(whose_best('3/input1.txt'))
-    # print(whose_best('3/input2.txt'))
-    # print(whose_best('3/input3.txt'))
-    # print(whose_best('3/input4.txt'))
-    # print(whose_best('3/input5.txt'))
-    print(whose_best('3/input6.txt'))
-    # print(whose_best('3/input7.txt'))
+    print(whose_best('3/input1.txt')) # Alice
+    print(whose_best('3/input2.txt')) # Invalid
+    print(whose_best('3/input3.txt')) # Unknown
+    print(whose_best('3/input4.txt')) # Alice
+    print(whose_best('3/input5.txt')) # Elmer
+    print(whose_best('3/input6.txt')) # Carol
+    print(whose_best('3/input7.txt')) # Invalid
 
 if __name__ == "__main__":
     main()
