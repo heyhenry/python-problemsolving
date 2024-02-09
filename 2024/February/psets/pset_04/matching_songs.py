774a8f90aa01d880169928186786b47f004f18ec
heyhenry <henryyy3@hotmail.com>
2024-02-09 22:59:29 +0800

    in progress q4 pset4

---
+++ 2024/February/psets/pset_04/matching_songs.py
@@ -0,0 +1,47 @@
+def matching_songs(filename : str):
+
+    results = []
+
+    with open(filename, 'r') as file:
+
+        read_content = file.read()
+
+        if read_content:
+
+            songs = read_content.splitlines()
+
+            filter = []
+            
+            for c in songs[0]:
+                filter.append(c)
+            
+            stack = []
+
+            for song in range(1, len(songs)):           
+                if len(stack) != len(filter):
+                    stack += filter
+                for c in songs[song].lower():
+                    if len(stack) > 0 and c == stack[0]:
+                        stack.pop(0)
+                    elif len(stack) > 0 and c in stack and c == stack[0]:
+                        stack.clear()
+                        stack += filter
+                        stack.pop(0)
+                    elif len(stack) > 0 and c in stack:
+                        stack.clear()
+                        stack += filter
+                    print(stack)
+                if len(stack) == 0:
+                    results.append(songs[song])
+                        
+                
+
+    return results
+
+def main():
+    # print(matching_songs('4/input1.txt'))
+    # print(matching_songs('4/input2.txt'))
+    print(matching_songs('4/input3.txt'))
+
+if __name__ == "__main__":
+    main()
