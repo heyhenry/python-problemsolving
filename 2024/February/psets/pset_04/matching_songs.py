5e9beebb5b710493876214a6e0682f3c28a0435c
heyhenry <henryyy3@hotmail.com>
2024-02-10 12:59:21 +0800

    solved q4 pset04

--- 2024/February/psets/pset_04/matching_songs.py
+++ 2024/February/psets/pset_04/matching_songs.py
@@ -19,28 +19,28 @@ def matching_songs(filename : str):
 
             for song in range(1, len(songs)):           
                 if len(stack) != len(filter):
+                    stack.clear()
                     stack += filter
                 for c in songs[song].lower():
-                    if len(stack) > 0 and c == stack[0]:
+                    if len(stack) == 0:
+                        break
+                    elif len(stack) > 0 and c == stack[0]:
                         stack.pop(0)
-                    elif len(stack) > 0 and c in stack and c == stack[0]:
+                    elif len(stack) > 0 and c in filter and c == filter[0]:
                         stack.clear()
                         stack += filter
                         stack.pop(0)
-                    elif len(stack) > 0 and c in stack:
+                    elif len(stack) > 0 and c in filter and c != stack[0]:
                         stack.clear()
                         stack += filter
-                    print(stack)
                 if len(stack) == 0:
                     results.append(songs[song])
                         
-                
-
     return results
 
 def main():
-    # print(matching_songs('4/input1.txt'))
-    # print(matching_songs('4/input2.txt'))
+    print(matching_songs('4/input1.txt'))
+    print(matching_songs('4/input2.txt'))
     print(matching_songs('4/input3.txt'))
 
 if __name__ == "__main__":
