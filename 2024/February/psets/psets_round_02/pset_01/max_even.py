b184ff5ceb127090954322f743ff3d159e3ddf31
heyhenry <henryyy3@hotmail.com>
2024-02-10 13:06:24 +0800

    solved pset01 again

---
+++ 2024/February/psets/psets_round_02/pset_01/max_even.py
@@ -0,0 +1,27 @@
+"""
+Returns the maximum even number in `lst`, or `None` if none exists
+Note that negatives are even if the associated positive is
+Examples:
+max_even([1, 2, 3]) -> 2
+max_even([-4, 3, -1, -6]) -> -4
+max_even([1, 3, 5]) -> None
+max_even([0]) -> 0
+"""
+def max_even(lst : list[int]) -> int | None:
+
+    result = None
+
+    for i in lst:
+        if i % 2 == 0 and (result is None or result < i):
+            result = i
+
+    return result
+
+def main():
+    print(max_even([1, 2, 3]))
+    print(max_even([-4, 3, -1, -6]))
+    print(max_even([1, 3, 5]))
+    print(max_even([0]))
+
+if __name__ == "__main__":
+    main()
