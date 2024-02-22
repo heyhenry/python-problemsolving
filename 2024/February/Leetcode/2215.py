8c42ba2749277b15ff4e1a10dcdf2c59b309bc2e
heyhenry <henryyy3@hotmail.com>
2024-02-22 14:30:09 +0800

    Implemented slightly faster runtime for leetcode no.2215

--- 2024/February/Leetcode/2215.py
+++ 2024/February/Leetcode/2215.py
@@ -34,12 +34,15 @@ def findDifference(nums1 : list[int], nums2 : list[int]) -> list[list[int]]:
 
     ans = [[],[]]
 
-    for i in nums1:
-        if i not in nums2 and i not in ans[0]:
+    uniq_nums1 = list(set(nums1))
+    uniq_nums2 = list(set(nums2))
+
+    for i in uniq_nums1:
+        if i not in uniq_nums2 and i not in ans[0]:
             ans[0].append(i)
 
-    for i in nums2:
-        if i not in nums1 and i not in ans[1]:
+    for i in uniq_nums2:
+        if i not in uniq_nums1 and i not in ans[1]:
             ans[1].append(i)
 
     return ans
