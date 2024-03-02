0aa91d2fe09543a8ab6739ade5eecd811d250a1a
heyhenry <henryyy3@hotmail.com>
2024-03-02 20:24:44 +0800

    solved pset01

--- 2024/February/small_programs/leetcodes_todo.py
+++ 2024/February/small_programs/leetcodes_todo.py
@@ -4,16 +4,15 @@ A quick program to find out the leetcodes in January's directory that hasn't bee
 
 import os
 
-jan_leetcodes = os.listdir(r"E:\PYTHON\python-problemsolving\2024\January\leetcode")
-feb_leetcodes = os.listdir(r"E:\PYTHON\python-problemsolving\2024\February\Leetcode")
-feb_r02_leetcodes = os.listdir(r"E:\PYTHON\python-problemsolving\2024\February\Leetcode_round_two")
+comparison_file = os.listdir(r"E:\PYTHON\python-problemsolving\2024\January\leetcode")
+new_file = os.listdir(r"E:\PYTHON\python-problemsolving\2024\February\Leetcode")
 
 with open("todo_leetcodes.results", 'w') as nfile:
 
     nfile.write("Leetcodes Left from February/Leetcode: \n")
 
-    for i in range(len(feb_leetcodes)):
-        if i != len(feb_leetcodes) - 1 and feb_leetcodes[i] not in feb_r02_leetcodes:
-            nfile.write(feb_leetcodes[i] + '\n')
-        elif i == len(feb_leetcodes) - 1 and feb_leetcodes[i] not in feb_r02_leetcodes:
-            nfile.write(feb_leetcodes[i])
+    for i in range(len(comparison_file)):
+        if i != len(comparison_file) - 1 and comparison_file[i] not in new_file:
+            nfile.write(comparison_file[i] + '\n')
+        elif i == len(comparison_file) - 1 and comparison_file[i] not in new_file:
+            nfile.write(comparison_file[i])
