79ce07bc828670533548a9e45d48f67747bfabed
heyhenry <henryyy3@hotmail.com>
2024-03-03 09:08:57 +0800

    solved leetcode no.125

---
+++ 2024/March/Leetcode/125.py
@@ -0,0 +1,52 @@
+"""
+125. Valid Palindrome
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+"""
+def is_palindrome(s : str) -> bool:
+
+    clean_s = ''
+
+    for c in s:
+        if c.isdigit() or c.isalpha():
+            clean_s += c
+
+    clean_s = clean_s.lower()
+
+    return clean_s == clean_s[::-1]
+
+
+def main():
+    print(is_palindrome(s = "A man, a plan, a canal: Panama"))
+    print(is_palindrome(s = "race a car"))
+    print(is_palindrome(s = " "))
+
+if __name__ == "__main__":
+    main()
