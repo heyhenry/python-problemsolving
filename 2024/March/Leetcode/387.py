a640913ab8c1f72f5a0145824064cbc04179fc5d
heyhenry <henryyy3@hotmail.com>
2024-03-06 09:01:06 +0800

    solved leetcode no.387

---
+++ 2024/March/Leetcode/387.py
@@ -0,0 +1,55 @@
+"""
+387. First Unique Character in a String
+
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+ 
+Constraints:
+
+1 <= s.length <= 105
+s consists of only lowercase English letters.
+"""
+def first_uniq_char(s : str) -> int:
+    
+    n = len(s)
+    r = {}
+    result = -1
+
+    for c in range(n):
+        if s[c] not in r:
+            r[s[c]] = s.count(s[c])
+
+    remember_dat = ''
+
+    for key, val in r.items():
+        if val == 1:
+            remember_dat = key
+            break
+
+    for i in range(n):
+
+        if s[i] == remember_dat:
+            result = i
+            break
+
+    return result   
+
+def main():
+    print(first_uniq_char(s = "leetcode"))
+    print(first_uniq_char(s = "loveleetcode"))
+    print(first_uniq_char(s = "aabb"))
+
+if __name__ == "__main__":
+    main()
