2cf3bc4352d7f491892064b8f964ec131f103617
heyhenry <henryyy3@hotmail.com>
2024-03-16 07:20:47 +0800

    another perspective solution for no.141

--- 2024/March/Leetcode/141.py
+++ 2024/March/Leetcode/141.py
@@ -58,15 +58,33 @@ class Solution():
 
         return False
 
-head = ListNode(3)
+        # another perspective - not mine
+        # slow, fast = head
+
+        # while fast and fast.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
+
+        #     if slow == fast:
+        #         return True
+        
+        # return False
+
+# head = ListNode(3)
+# node2 = ListNode(2)
+# node3 = ListNode(0)
+# node4 = ListNode(-4)
+
+# head.next = node2
+# node2.next = node3
+# node3.next = node4
+# node4.next = node2
+
+head = ListNode(1)
 node2 = ListNode(2)
-node3 = ListNode(0)
-node4 = ListNode(-4)
 
 head.next = node2
-node2.next = node3
-node3.next = node4
-node4.next = node2
+node2.next = node2
 
 solution = Solution()
 
