2801e26a4a7bc68ec4a62d52189e1362891b1216
heyhenry <henryyy3@hotmail.com>
2024-03-16 07:08:40 +0800

    solved leetcode no.141

---
+++ 2024/March/Leetcode/141.py
@@ -0,0 +1,73 @@
+"""
+141. Linked List Cycle
+
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+ 
+
+Example 1:
+
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+Example 2:
+
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+Example 3:
+
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+ 
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+ 
+
+Follow up: Can you solve it using O(1) (i.e. constant) memory?
+"""
+class ListNode():
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class Solution():
+    def has_cycle(self, head : ListNode) -> bool:
+        if not head or head.next:
+            return False
+        
+        slow_ptr = head
+        fast_ptr = head.next
+
+        while fast_ptr.next and fast_ptr.next.next:
+            if slow_ptr == fast_ptr:
+                return True
+            slow_ptr = slow_ptr.next
+            fast_ptr = fast_ptr.next.next
+
+        return False
+
+head = ListNode(3)
+node2 = ListNode(2)
+node3 = ListNode(0)
+node4 = ListNode(-4)
+
+head.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node2
+
+solution = Solution()
+
+print(solution.has_cycle(head))
