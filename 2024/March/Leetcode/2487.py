cba50b28db3ea47a063b895cf5c4670c8f0f2ec9
heyhenry <henryyy3@hotmail.com>
2024-03-15 15:39:11 +0800

    solved leetcode no.2487

--- 2024/March/Leetcode/2487.py
+++ 2024/March/Leetcode/2487.py
@@ -29,44 +29,79 @@ The number of the nodes in the given list is in the range [1, 105].
 1 <= Node.val <= 105
 """
 class ListNode():
-    def __init__(self, val):
+    def __init__(self, val=0,next=None):
         self.val = val
-        self.next = None
+        self.next = next
 
 class Solution():
     def remove_nodes(self, head : ListNode) -> ListNode:
 
-        current_node = head
-        lst = []
+        # current_node = head
+        # lst = []
         
-        while current_node:
-            lst.append(current_node.val)
-            current_node = current_node.next
+        # while current_node:
+        #     lst.append(current_node.val)
+        #     current_node = current_node.next
         
-        n = len(lst)
+        # n = len(lst)
 
-        to_remove = []
+        # to_remove = []
 
-        for i in range(n):
-            for j in range(i+1, n):
-                if lst[i] < lst[j] and i < j:
-                    if to_remove.count(lst[i]) < lst.count(lst[i]):
-                        to_remove.append(lst[i])
-                    break
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         if lst[i] < lst[j] and i < j:
+        #             if to_remove.count(lst[i]) < lst.count(lst[i]):
+        #                 to_remove.append(lst[i])
+        #             break
                         
-        current_node = head
-        prev_node = None            
-        while current_node:
-            if current_node.val in to_remove:
-                to_remove.remove(current_node.val)
-                if prev_node:
-                    prev_node.next = current_node.next
-                else:
-                    head = current_node.next
-            else:
-                prev_node = current_node
-            current_node = current_node.next
+        # current_node = head
+        # prev_node = None            
+        # while current_node:
+        #     if current_node.val in to_remove:
+        #         to_remove.remove(current_node.val)
+        #         if prev_node:
+        #             prev_node.next = current_node.next
+        #         else:
+        #             head = current_node.next
+        #     else:
+        #         prev_node = current_node
+        #     current_node = current_node.next
     
+        # return head     
+
+        curr = head
+        prev = None
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        head = prev
+
+        curr = head
+        prev = None
+        curr_max = head.val
+        while curr:
+            if curr.val < curr_max:
+                if prev:
+                    prev.next = curr.next
+                else:
+                    head = curr.next
+            else:
+                if curr.val > curr_max:
+                    curr_max = curr.val
+                prev = curr
+            curr = curr.next
+        
+        print(head.val)
+        curr = head
+        prev = None
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        head = prev
         return head
 
     def display(self, head):
@@ -78,7 +113,7 @@ class Solution():
 
 solution = Solution()
 
-data = [138,466,216,67,642,978,264,136,463,331,60,600,223,275,856,809,167,101,846,165,575,276,409,590,733,200,839,515,852,615,8,584,250,337,537,63,797,900,670,636,112,701,334,422,780,552,912,506,313,474,183,792,822,661,37,164,601,271,902,792,501,184,559,140,506,94,161,167,622,288,457,953,700,464,785,203,729,725,422,76,191,195,157,854,730,577,503,401,517,692,42,135,823,883,255,111,334,365,513,338,65,600,926,607,193,763,366,674,145,229,700,11,984,36,185,475,204,604,191,898,876,762,654,770,774,575,276,165,610,649,235,749,440,607,962,747,891,943,839,403,655,22,705,416,904,765,905,574,214,471,451,774,41,365,703,895,327,879,414,821,363,30,130,14,754,41,494,548,76,825,899,499,188,982,8,890,563,438,363,32,482,623,864,161,962,678,414,659,612,332,164,580,14,633,842,969,792,777,705,436,750,501,395,342,838,493,998,112,660,961,943,721,480,522,133,129,276,362,616,52,117,300,274,862,487,715,272,232,543,275,68,144,656,623,317,63,908,565,880,12,920,467,559,91,698]
+data = [5,2,13,3,8]
 
 head = ListNode(data[0])
 current_node = head
