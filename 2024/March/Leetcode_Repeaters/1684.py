a843bbd49cebc9b250866c4d0c09f20e53f2771f
heyhenry <henryyy3@hotmail.com>
2024-03-18 07:27:56 +0800

    solved leetcode no.1684

---
+++ 2024/March/Leetcode_Repeaters/1684.py
@@ -0,0 +1,53 @@
+"""
+1684. Count the Number of Consistent Strings
+
+You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+
+Return the number of consistent strings in the array words.
+
+ 
+
+Example 1:
+
+Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+Output: 2
+Explanation: Strings "aaab" and "baa" are consistent since they only contain characters 'a' and 'b'.
+Example 2:
+
+Input: allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"]
+Output: 7
+Explanation: All strings are consistent.
+Example 3:
+
+Input: allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
+Output: 4
+Explanation: Strings "cc", "acd", "ac", and "d" are consistent.
+ 
+
+Constraints:
+
+1 <= words.length <= 104
+1 <= allowed.length <= 26
+1 <= words[i].length <= 10
+The characters in allowed are distinct.
+words[i] and allowed contain only lowercase English letters.
+"""
+def count_consistent_strings(allowed : str, words : list[str]) -> int:
+
+    fakes = 0
+
+    for row in words:
+        for i in row:
+            if i not in allowed:
+                fakes += 1
+                break
+
+    return len(words) - fakes
+
+def main():
+    print(count_consistent_strings(allowed = "ab", words = ["ad","bd","aaab","baa","badab"]))
+    print(count_consistent_strings(allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"]))
+    print(count_consistent_strings(allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]))
+
+if __name__ == "__main__":
+    main()
