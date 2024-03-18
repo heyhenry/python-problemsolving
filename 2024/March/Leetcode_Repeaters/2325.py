bd8f5e0252951142c67871bd1321f580829c1e80
heyhenry <henryyy3@hotmail.com>
2024-03-18 17:02:52 +0800

    solved leetcode no.2325

---
+++ 2024/March/Leetcode_Repeaters/2325.py
@@ -0,0 +1,66 @@
+"""
+2325. Decode the Message
+
+You are given the strings key and message, which represent a cipher key and a secret message, respectively. The steps to decode message are as follows:
+
+Use the first appearance of all 26 lowercase English letters in key as the order of the substitution table.
+Align the substitution table with the regular English alphabet.
+Each letter in message is then substituted using the table.
+Spaces ' ' are transformed to themselves.
+For example, given key = "happy boy" (actual key would have at least one instance of each letter in the alphabet), we have the partial substitution table of ('h' -> 'a', 'a' -> 'b', 'p' -> 'c', 'y' -> 'd', 'b' -> 'e', 'o' -> 'f').
+Return the decoded message.
+
+ 
+
+Example 1:
+
+
+Input: key = "the quick brown fox jumps over the lazy dog", message = "vkbs bs t suepuv"
+Output: "this is a secret"
+Explanation: The diagram above shows the substitution table.
+It is obtained by taking the first appearance of each letter in "the quick brown fox jumps over the lazy dog".
+Example 2:
+
+
+Input: key = "eljuxhpwnyrdgtqkviszcfmabo", message = "zwx hnfx lqantp mnoeius ycgk vcnjrdb"
+Output: "the five boxing wizards jump quickly"
+Explanation: The diagram above shows the substitution table.
+It is obtained by taking the first appearance of each letter in "eljuxhpwnyrdgtqkviszcfmabo".
+ 
+
+Constraints:
+
+26 <= key.length <= 2000
+key consists of lowercase English letters and ' '.
+key contains every letter in the English alphabet ('a' to 'z') at least once.
+1 <= message.length <= 2000
+message consists of lowercase English letters and ' '.
+"""
+def decode_message(key : str, message : str) -> str:
+
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    a = {}
+    key = key.replace(' ','')
+    n = len(key)
+    counter = 0
+    result = ''
+
+    for i in range(n):
+        if key[i] not in a:
+            a[key[i]] = alphabet[counter]
+            counter += 1
+
+    for i in message:
+        if i.isalpha():
+            result += a[i]
+        else:
+            result += i
+
+    return result
+
+def main():
+    print(decode_message(key = "the quick brown fox jumps over the lazy dog", message = "vkbs bs t suepuv"))
+    print(decode_message(key = "eljuxhpwnyrdgtqkviszcfmabo", message = "zwx hnfx lqantp mnoeius ycgk vcnjrdb"))
+
+if __name__ == "__main__":
+    main()
