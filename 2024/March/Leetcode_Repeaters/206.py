5d808721bd914f62a5d1e24c77669284795d8e85
heyhenry <henryyy3@hotmail.com>
2024-03-18 13:28:50 +0800

    solved leetcode no.206

---
+++ 2024/March/Leetcode_Repeaters/206.py
@@ -0,0 +1,72 @@
+"""
+206. Reverse Linked List
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+ 
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+"""
+class ListNode():
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class Solution():
+    def reverse_list(self, head : ListNode) -> ListNode:
+
+        curr = head
+        prev = None
+
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        head = prev
+        return head
+
+    def display(self, head):
+        curr = head
+        while curr:
+            print(curr.val, end=" -> ")
+            curr = curr.next
+        print("None")
+
+head = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(4)
+node5 = ListNode(5)
+
+head.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+solution = Solution()
+
+head = solution.reverse_list(head)
+solution.display(head)
