13b96ec864c81f53edfede5c95342064d9b2f18b
heyhenry <henryyy3@hotmail.com>
2024-03-18 13:22:14 +0800

    solved leetcode no.203

---
+++ 2024/March/Leetcode_Repeaters/203.py
@@ -0,0 +1,79 @@
+"""
+203. Remove Linked List Elements
+
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 104].
+1 <= Node.val <= 50
+0 <= val <= 50
+"""
+class ListNode():
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class Solution():
+    def remove_elements(self, head : ListNode, val : int) -> ListNode:
+
+        curr = head
+        prev = None
+
+        while curr:
+            if curr.val == val:
+                if prev:
+                    prev.next = curr.next
+                else:
+                    head = curr.next
+            else:
+                prev = curr
+            curr = curr.next
+        
+        return head
+    
+    def display(self, head):
+
+        curr = head
+        
+        while curr:
+            print(curr.val, end=" -> ")
+            curr = curr.next
+        print("None")
+
+head = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(6)
+node4 = ListNode(3)
+node5 = ListNode(4)
+node6 = ListNode(5)
+node7 = ListNode(6)
+
+head.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = node6
+node6.next = node7
+
+solution = Solution()
+
+head = solution.remove_elements(head, 6)
+solution.display(head)
