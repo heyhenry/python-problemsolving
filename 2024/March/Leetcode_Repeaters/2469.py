4fa7a7c73fc22cc1905df89015399caea084ddcb
heyhenry <henryyy3@hotmail.com>
2024-03-19 09:42:10 +0800

    solved leetcode no.2469

---
+++ 2024/March/Leetcode_Repeaters/2469.py
@@ -0,0 +1,49 @@
+"""
+2469. Convert the Temperature
+
+You are given a non-negative floating point number rounded to two decimal places celsius, that denotes the temperature in Celsius.
+
+You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
+
+Return the array ans. Answers within 10-5 of the actual answer will be accepted.
+
+Note that:
+
+Kelvin = Celsius + 273.15
+Fahrenheit = Celsius * 1.80 + 32.00
+ 
+
+Example 1:
+
+Input: celsius = 36.50
+Output: [309.65000,97.70000]
+Explanation: Temperature at 36.50 Celsius converted in Kelvin is 309.65 and converted in Fahrenheit is 97.70.
+Example 2:
+
+Input: celsius = 122.11
+Output: [395.26000,251.79800]
+Explanation: Temperature at 122.11 Celsius converted in Kelvin is 395.26 and converted in Fahrenheit is 251.798.
+ 
+
+Constraints:
+
+0 <= celsius <= 1000
+"""
+def convert_to_kelvin(celsius : float) -> float:
+
+    return celsius + 273.15
+
+def convert_to_fahrenheit(celsius : float) -> float:
+
+    return celsius * 1.80 + 32.00
+
+def convert_temperature(celsius : float) -> list[float]:
+
+    return [convert_to_kelvin(celsius), convert_to_fahrenheit(celsius)]
+
+def main():
+    print(convert_temperature(celsius = 36.50))
+    print(convert_temperature(celsius = 122.11))
+
+if __name__ == "__main__":
+    main()
