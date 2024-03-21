0d77aed8b14a33dd266c7cdb5721bb97602815d2
heyhenry <henryyy3@hotmail.com>
2024-03-21 10:15:42 +0800

    solved leetcode no.2942

---
+++ 2024/March/Leetcode_Repeaters/2942.py
@@ -0,0 +1,53 @@
+"""
+2942. Find Words Containing Character
+
+You are given a 0-indexed array of strings words and a character x.
+
+Return an array of indices representing the words that contain the character x.
+
+Note that the returned array may be in any order.
+
+ 
+
+Example 1:
+
+Input: words = ["leet","code"], x = "e"
+Output: [0,1]
+Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.
+Example 2:
+
+Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
+Output: [0,2]
+Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2.
+Example 3:
+
+Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
+Output: []
+Explanation: "z" does not occur in any of the words. Hence, we return an empty array.
+ 
+
+Constraints:
+
+1 <= words.length <= 50
+1 <= words[i].length <= 50
+x is a lowercase English letter.
+words[i] consists only of lowercase English letters.
+"""
+def find_words_containing(words : list[str], x : str) -> list[int]:
+
+    n = len(words)
+    result = []
+
+    for i in range(n):
+        if x in words[i]:
+            result.append(i)
+
+    return result
+
+def main():
+    print(find_words_containing(words = ["leet","code"], x = "e"))
+    print(find_words_containing(words = ["abc","bcd","aaaa","cbc"], x = "a"))
+    print(find_words_containing(words = ["abc","bcd","aaaa","cbc"], x = "z"))
+
+if __name__ == "__main__":
+    main()
