a7d556134eb2b35a74a7b6a044c25bb179e82c70
heyhenry <henryyy3@hotmail.com>
2024-04-02 14:08:21 +0800

    my progress on leetcode no.2 (test cases that have carry over do not pass)

--- 2024/April/Leetcode/2.py
+++ 2024/April/Leetcode/2.py
@@ -37,7 +37,39 @@ class ListNode:
 class Solution:
     def add_two_numbers(self, l1 : ListNode, l2 : ListNode) -> ListNode:
 
-        pass
+        first_num = ''
+        second_num = ''
+        first = l1
+        second = l2
+
+        while first:
+            first_num += str(first.val)
+            first = first.next
+        while second:
+            second_num += str(second.val)
+            second = second.next
+
+        ans = int(first_num) + int(second_num)
+        ans = str(ans)[::-1]
+
+        head = ListNode(ans[0])
+        counter = 1
+        while counter < len(ans):
+            new_node = ListNode(ans[counter]) 
+            current = head
+            while current.next:
+                current = current.next
+            current.next = new_node
+            counter += 1
+
+        # print(head.val)
+        # curr = head
+        # while curr:
+        #     print(curr.val, end = " -> ")
+        #     curr = curr.next
+        # print("None")
+
+        return head
     
     def display(self, l1, l2):
         l1_current = l1
@@ -70,4 +102,5 @@ l2_node3 = ListNode(4)
 l2_head.next = l2_node2
 l2_node2.next = l2_node3
 
-solution.display(l1_head, l2_head)
+# solution.display(l1_head, l2_head)
+solution.add_two_numbers(l1_head, l2_head)
