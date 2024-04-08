ab1ad5f1d720e74569c1a54d9312d30c842d3a57
heyhenry <henryyy3@hotmail.com>
2024-04-09 07:36:19 +0800

    solved leetcode no.1832

---
+++ 2024/April/Leetcode/1832.py
@@ -0,0 +1,41 @@
+"""
+1832. Check if the Sentence Is Pangram
+
+A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+Output: true
+Explanation: sentence contains at least one of every letter of the English alphabet.
+Example 2:
+
+Input: sentence = "leetcode"
+Output: false
+ 
+
+Constraints:
+
+1 <= sentence.length <= 1000
+sentence consists of lowercase English letters.
+"""
+def check_if_paragram(sentence : str) -> bool:
+
+    alphabet = set('abcdefghijklmnopqrstuvwxyz')
+    upd = set(sentence)
+
+    if alphabet.difference(upd):
+        return False
+    
+    return True
+
+def main():
+    print(check_if_paragram(sentence = "thequickbrownfoxjumpsoverthelazydog"))
+    print(check_if_paragram(sentence = "leetcode"))
+
+if __name__ == "__main__":
+    main()
