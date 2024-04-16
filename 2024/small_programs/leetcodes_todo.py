018e4785e58250fcc9dee1cc141d9a8a89d9e647
heyhenry <henryyy3@hotmail.com>
2024-04-17 00:22:26 +0800

    updated leetcodes results file logic to count appropriate completion

--- 2024/small_programs/leetcodes_todo.py
+++ 2024/small_programs/leetcodes_todo.py
@@ -6,11 +6,23 @@ import os
 
 comparison_file = os.listdir(r"E:\PYTHON\python-problemsolving\2024\March\Leetcode")
 new_file = os.listdir(r"E:\PYTHON\python-problemsolving\2024\April\Leetcode")
+leetcodes_file = r"E:\PYTHON\python-problemsolving\2024\small_programs\todo_leetcodes.results"
 
-with open("todo_leetcodes.results", 'w') as nfile:
+count = 0
+
+with open(leetcodes_file, 'r') as file:
+    read_content = file.read()
+    if read_content:
+        splitlines = read_content.splitlines()
+        
+        for row in splitlines:
+            if row[0].isnumeric():
+                count += 1
+
+with open(leetcodes_file, 'w') as nfile:
 
     nfile.write("Leetcodes Questions To Do: \n")
-    nfile.write("Leetcodes Left: " + str(len(comparison_file) - len(new_file)) + "\n")
+    nfile.write("Leetcodes Left: " + str(count) + "\n")
 
     for i in range(len(comparison_file)):
         if i != len(comparison_file) - 1 and comparison_file[i] not in new_file:
