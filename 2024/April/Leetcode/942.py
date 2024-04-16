174ee3e2aff2d0dd9cd9891f04a3bf5ed459aeae
heyhenry <henryyy3@hotmail.com>
2024-04-16 23:54:56 +0800

    solved leetcode no.942

---
+++ 2024/April/Leetcode/942.py
@@ -0,0 +1,57 @@
+"""
+942. DI String Match
+
+A permutation perm of n + 1 integers of all the integers in the range [0, n] can be represented as a string s of length n where:
+
+s[i] == 'I' if perm[i] < perm[i + 1], and
+s[i] == 'D' if perm[i] > perm[i + 1].
+Given a string s, reconstruct the permutation perm and return it. If there are multiple valid permutations perm, return any of them.
+
+ 
+
+Example 1:
+
+Input: s = "IDID"
+Output: [0,4,1,3,2]
+Example 2:
+
+Input: s = "III"
+Output: [0,1,2,3]
+Example 3:
+
+Input: s = "DDI"
+Output: [3,2,0,1]
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is either 'I' or 'D'.
+"""
+def di_string_match(s : str) -> list[int]:
+
+    perm = []
+    n = len(s)+1
+    left = 0
+    right = n-1
+
+    for i in range(n):
+        if i != n-1 and s[i] == 'I':
+            perm.append(left)
+            left += 1
+        elif i != n-1 and s[i] == 'D':
+            perm.append(right)
+            right -= 1
+
+    perm.append(left)
+
+    return perm
+
+
+def main():
+    print(di_string_match(s = "IDID"))
+    print(di_string_match(s = "III"))
+    print(di_string_match(s = "DDI"))
+
+if __name__ == "__main__":
+    main()
