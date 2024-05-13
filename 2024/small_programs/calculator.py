8d6b66fb5730483408318bd8af2babf1278ddbe7
heyhenry <henryyy3@hotmail.com>
2024-05-13 10:30:31 +0800

    updated naming

--- 2024/small_programs/calculator.py
+++ 2024/small_programs/calculator.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.title("Fuck man")
+window.title("Calculator App")
 window.geometry("320x500")
 
 num1_var = tk.IntVar(window, '')
@@ -22,10 +22,15 @@ second_num = tk.Label(window, text="second Num: ")
 num2_entry = tk.Entry(window, textvariable=num2_var)
 calc_btn = tk.Button(window, text = "Calculate", command=calculate)
 
-first_num.pack()
-num1_entry.pack()
-second_num.pack()
-num2_entry.pack()
-calc_btn.pack()
+# first_num.pack()
+# num1_entry.pack()
+# second_num.pack()
+# num2_entry.pack()
+# calc_btn.pack()
+first_num.grid(row=0, column=0)
+num1_entry.grid(row=0, column=1)
+second_num.grid(row=1, column=0)
+num2_entry.grid(row=1, column=1)
+calc_btn.grid(row=2, column=1)
 
 window.mainloop()
