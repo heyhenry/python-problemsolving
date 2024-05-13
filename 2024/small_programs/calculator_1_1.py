ff0121c0af6ae6581bbadb5fa40fb34973588b07
heyhenry <henryyy3@hotmail.com>
2024-05-14 01:18:59 +0800

    Added additional calc functions

--- 2024/small_programs/calculator_1_1.py
+++ 2024/small_programs/calculator_1_1.py
@@ -14,27 +14,41 @@ num1_var = tk.IntVar(display_frame, '')
 num2_var = tk.IntVar(display_frame, '')
 result_var = tk.IntVar(display_frame)
 
+# functions
 def add():
-
     result_var.set(num1_var.get() + num2_var.get())
-    
-    # testing
-    result = result_var.get()
-    print(result)
+
+def subtract():
+    result_var.set(num1_var.get() - num2_var.get())
+
+def multiply():
+    result_var.set(num1_var.get() * num2_var.get())
+
+def divide():
+    result_var.set(num1_var.get() / num2_var.get())
 
 # display frame
 output_label = tk.Label(display_frame, textvariable=result_var)
 num1_input = tk.Entry(display_frame, textvariable=num1_var)
 num2_input = tk.Entry(display_frame, textvariable=num2_var)
-add_btn = tk.Button(input_frame, text="Pookie", command=add)
+add_btn = tk.Button(input_frame, text="Add", command=add)
+subtract_btn = tk.Button(input_frame, text="Subtract", command=subtract) 
+multiply_btn = tk.Button(input_frame, text="Multiply", command=multiply)
+divide_btn = tk.Button(input_frame, text="Divide", command=divide)
 
 # packing
 display_frame.pack()
 input_frame.pack()
+
 output_label.pack()
+
 num1_input.pack()
 num2_input.pack()
+
 add_btn.pack()
+subtract_btn.pack()
+multiply_btn.pack()
+divide_btn.pack()
 
 # the run
 window.mainloop()
