6e7a1d27bdf8e83f7780a6de3bb489512d69c09c
heyhenry <henryyy3@hotmail.com>
2024-05-13 10:24:03 +0800

    Created basic addition calculator.

--- 2024/small_programs/calculator.py
+++ 2024/small_programs/calculator.py
@@ -1,11 +1,31 @@
 import tkinter as tk
 
 window = tk.Tk()
+window.title("Fuck man")
+window.geometry("320x500")
 
-window.title('Hans Calculator')
-window.geometry('300x500') # recreated same size as the calc app
+num1_var = tk.IntVar()
+num2_var = tk.IntVar()
 
+def calculate():
 
+    num1 = num1_var.get()
+    num2 = num2_var.get()
 
+    result = num1 + num2
+
+    print("The sum is: " + str(result))
+
+first_num = tk.Label(window, text="First Num: ")
+num1_entry = tk.Entry(window, textvariable=num1_var)
+second_num = tk.Label(window, text="second Num: ")
+num2_entry = tk.Entry(window, textvariable=num2_var)
+calc_btn = tk.Button(window, text = "Calculate", command=calculate)
+
+first_num.pack()
+num1_entry.pack()
+second_num.pack()
+num2_entry.pack()
+calc_btn.pack()
 
 window.mainloop()
