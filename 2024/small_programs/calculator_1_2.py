195e3639ff0077823c811a794e8bb06b725e456b
heyhenry <henryyy3@hotmail.com>
2024-05-14 11:42:09 +0800

    continuously addable functionality.

--- 2024/small_programs/calculator_1_2.py
+++ 2024/small_programs/calculator_1_2.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 
+# global var
+overall_num = 0
+calculate = True
+
 # initial window setup
 window = tk.Tk()
 window.title("Calculator App ver.1.2")
@@ -13,20 +17,28 @@ input_frame = tk.Frame(window)
 num_var = tk.IntVar(display_frame)
 result_var = tk.IntVar(display_frame)
 
+# functions
+def add_num(num):
+    global overall_num
+    overall_num += num
+
+def calculating():
+    result_var.set(overall_num)
+
 # display_frame content
 output = tk.Label(display_frame, textvariable=result_var)
 
 # input_frame content
-numpad_one = tk.Button(input_frame, text='1')
-numpad_two = tk.Button(input_frame, text='2')
-numpad_three = tk.Button(input_frame, text='3')
-calculate_btn = tk.Button(input_frame, text="Calculate")
+numpad_one = tk.Button(input_frame, text='1', command=lambda: add_num(1))
+numpad_two = tk.Button(input_frame, text='2', command=lambda: add_num(2))
+numpad_three = tk.Button(input_frame, text='3', command=lambda: add_num(3))
+calculate_btn = tk.Button(input_frame, text="Calculate", command=calculating)
 
 # packaging
 display_frame.pack()
 input_frame.pack()
 
-output.grid(row=0, column=0, )
+output.pack()
 
 numpad_one.grid(row=0, column=0)
 numpad_two.grid(row=0, column=1)
