a952d789d533f5ef1ef1105e17d32e2a35365182
heyhenry <henryyy3@hotmail.com>
2024-05-20 12:56:27 +0800

    updates result in realtime before calculate

--- 2024/small_programs/calculator_1_4.py
+++ 2024/small_programs/calculator_1_4.py
@@ -25,14 +25,18 @@ class CalculatorApp(tk.Tk):
         numpad_two = tk.Button(self.input_frame, text='2', command=lambda: self.add_num(2))
         numpad_three = tk.Button(self.input_frame, text='3', command=lambda: self.add_num(3))
         calculate_btn = tk.Button(self.input_frame, text='Calculate', command=self.calculate)
-    
+
         numpad_one.grid(row=0, column=0)
         numpad_two.grid(row=0, column=1)
         numpad_three.grid(row=0, column=2)
         calculate_btn.grid(row=1, columnspan=3)
+        
 
     def add_num(self, num):
         self.overall_num += num
+
+        # update the result_var in realtime
+        self.result_var.set(self.overall_num)
 
     def calculate(self):
         self.result_var.set(self.overall_num)
