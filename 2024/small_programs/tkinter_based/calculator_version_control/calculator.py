90f501a349537262ad4628900187e3abb4d0f523
heyhenry <henryyy3@hotmail.com>
2024-05-24 18:32:07 +0800

    updated entry widget input size

--- 2024/small_programs/tkinter_based/calculator_version_control/calculator.py
+++ 2024/small_programs/tkinter_based/calculator_version_control/calculator.py
@@ -3,7 +3,12 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Calculator App")
 
+data = tk.StringVar()
 
+data_entry = tk.Entry(window, textvariable=data, borderwidth=5, width=25, font=('Arial', 22)).grid(columnspan=3, padx=5, pady=5)
+add_btn = tk.Button(window, text='+', relief='groove').grid(row=1, column=0,sticky='nswe')
+sub_btn = tk.Button(window, text='-', relief='groove').grid(row=1, column=1, sticky='nswe')
+times_btn = tk.Button(window, text='*', relief='groove').grid(row=1, column=2, sticky='nswe')
 
 
 
