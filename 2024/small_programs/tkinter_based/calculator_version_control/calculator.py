033d0ce2c46ff3ba3418d6cf01c705be8c2102de
heyhenry <henryyy3@hotmail.com>
2024-05-24 18:48:51 +0800

    created numpad, operators and del/equal buttons. Proper grid layout applied.

--- 2024/small_programs/tkinter_based/calculator_version_control/calculator.py
+++ 2024/small_programs/tkinter_based/calculator_version_control/calculator.py
@@ -4,11 +4,31 @@ window = tk.Tk()
 window.title("Calculator App")
 
 data = tk.StringVar()
+btn_params = {'font':('sans-serif',16), 'relief': 'groove'}
 
-data_entry = tk.Entry(window, textvariable=data, borderwidth=5, width=25, font=('Arial', 22)).grid(columnspan=3, padx=5, pady=5)
-add_btn = tk.Button(window, text='+', relief='groove').grid(row=1, column=0,sticky='nswe')
-sub_btn = tk.Button(window, text='-', relief='groove').grid(row=1, column=1, sticky='nswe')
-times_btn = tk.Button(window, text='*', relief='groove').grid(row=1, column=2, sticky='nswe')
+# input display
+data_entry = tk.Entry(window, textvariable=data, borderwidth=5, width=25, font=('sans-serif', 22)).grid(columnspan=3, padx=5, pady=5)
+
+# operators
+add_btn = tk.Button(window, btn_params, text='+').grid(row=1, column=0,sticky='nswe')
+sub_btn = tk.Button(window, btn_params, text='-').grid(row=1, column=1, sticky='nswe')
+times_btn = tk.Button(window, btn_params, text='*').grid(row=1, column=2, sticky='nswe')
+
+# numpad
+numpad_one = tk.Button(window, btn_params, text='1').grid(row=2, column=0, sticky='nswe')
+numpad_two = tk.Button(window, btn_params, text='2').grid(row=2, column=1, sticky='nswe')
+numpad_three = tk.Button(window, btn_params, text='3').grid(row=2, column=2, sticky='nswe')
+numpad_four = tk.Button(window, btn_params, text='4').grid(row=3, column=0, sticky='nswe')
+numpad_five = tk.Button(window, btn_params, text='5').grid(row=3, column=1, sticky='nswe')
+numpad_six = tk.Button(window, btn_params, text='6').grid(row=3, column=2, sticky='nswe')
+numpad_seven = tk.Button(window, btn_params, text='7').grid(row=4, column=0, sticky='nswe')
+numpad_eight = tk.Button(window, btn_params, text='8').grid(row=4, column=1, sticky='nswe')
+numpad_nine = tk.Button(window, btn_params, text='9').grid(row=4, column=2, sticky='nswe')
+numpad_ten = tk.Button(window, btn_params, text='0').grid(row=5, column=1, sticky='nswe')
+
+# del / equal
+del_btn = tk.Button(window, btn_params, text='DEL').grid(row=5,column=0, sticky='nswe')
+equal_btn = tk.Button(window, btn_params, text='=').grid(row=5, column=2, sticky='nswe')
 
 
 
--- 2024/small_programs/tkinter_based/calculator_version_control/test.py
+++ 2024/small_programs/tkinter_based/calculator_version_control/test.py
@@ -1,28 +1,27 @@
-# alternative to using the global keyword multiple times (a potential solution/alternative)
-# sample playground
+# # alternative to using the global keyword multiple times (a potential solution/alternative)
+# # sample playground
 
-text = ''
+# text = ''
 
-def func_a(text, char):
-    text += char
-    return text
+# def func_a(text, char):
+#     text += char
+#     return text
 
-def func_b(text, char):
-    text += char
-    return text
+# def func_b(text, char):
+#     text += char
+#     return text
 
-while True:
-    print("RESULT: " + text)
+# while True:
+#     print("RESULT: " + text)
 
-    user_input = int(input("Type 1 or 2: "))
+#     user_input = int(input("Type 1 or 2: "))
 
-    if user_input == 1:
-        text = func_a(text, 'a')
-    elif user_input == 2:
-        text = func_b(text, 'b')
-    else:
-        break
-
+#     if user_input == 1:
+#         text = func_a(text, 'a')
+#     elif user_input == 2:
+#         text = func_b(text, 'b')
+#     else:
+#         break
 
 
 
