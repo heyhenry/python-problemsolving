b5be12a5f94b242940fb69a80535201ab543bd58
heyhenry <henryyy3@hotmail.com>
2025-11-29 11:35:20 +0800

    Leetcode (November 2025): Resolved two_sum using double for loop method.

--- 2025/November/leetcode/two_sum.py
+++ 2025/November/leetcode/two_sum.py
@@ -1,11 +1,26 @@
 class Solution:
     def two_sum(self, nums : list[int], target : int) -> list[int]:
-        size = len(nums)
-        for i in range(size):
-            for j in range(i+1, size):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        # method one - brute force
+        # size = len(nums)
+        # for i in range(size):
+        #     for j in range(i+1, size):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
         
+        # method 2 - linear or someshit
+        num_map = {}
+        size = len(nums)
+        
+        for i in range(size):
+            num_map[nums[i]] = i
+
+        for i in range(size):
+            complement = target - nums[i]
+            if complement in num_map and num_map[complement] != i:
+                return [num_map[complement], i]
+            
+        # not needed as question maker guarantess match to be found, but still good practice.
+        return []
 
 solution = Solution()
 
