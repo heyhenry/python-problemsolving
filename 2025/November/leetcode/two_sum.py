07c2a677b70cad866676246827539e067f282c84
heyhenry <henryyy3@hotmail.com>
2025-11-30 01:35:35 +0800

    Leetcode (November 2025): Resolved all 3 different methods.

--- 2025/November/leetcode/two_sum.py
+++ 2025/November/leetcode/two_sum.py
@@ -1,36 +1,31 @@
 class Solution:
     def two_sum(self, nums : list[int], target : int) -> list[int]:
-        # method one - brute force
+        # 1st method - brute force
         # size = len(nums)
         # for i in range(size):
         #     for j in range(i+1, size):
         #         if nums[i] + nums[j] == target:
         #             return [i, j]
-        
-        # # method 2 - double loop solution
+
+        # 2nd method - double for loop
         # num_map = {}
         # size = len(nums)
-        
         # for i in range(size):
         #     num_map[nums[i]] = i
-
         # for i in range(size):
         #     complement = target - nums[i]
         #     if complement in num_map and num_map[complement] != i:
         #         return [num_map[complement], i]
             
-        # # not needed as question maker guarantess match to be found, but still good practice.
-        # return []
-
-        # method 3 - single loop solution
-        num_map = {}
+        # 3rd method - single for loop 
         size = len(nums)
+        num_map = {}
+        
         for i in range(size):
             complement = target - nums[i]
             if complement in num_map:
                 return [num_map[complement], i]
             num_map[nums[i]] = i
-        return []
 
 solution = Solution()
 
@@ -39,6 +34,6 @@ test_case_2 = ([3,2,4], 6)
 test_case_3 = ([3,3], 6)
 
 print(solution.two_sum(test_case_1[0], test_case_1[1])) # [0,1]
-print(solution.two_sum(test_case_2[0], test_case_2[1])) # [1,2]
-print(solution.two_sum(test_case_3[0], test_case_3[1])) # [0,1]
+# print(solution.two_sum(test_case_2[0], test_case_2[1])) # [1,2]
+# print(solution.two_sum(test_case_3[0], test_case_3[1])) # [0,1]
 
