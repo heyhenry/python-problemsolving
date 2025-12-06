1ceb0f0b6907ce343e4e5a95ac242d71ed9e3cf0
heyhenry <henryyy3@hotmail.com>
2025-12-06 23:09:58 +0800

    Leetcode: Solved no.26 Remove Duplicates from Sorted Array.

---
+++ 2025/November/leetcode/remove_duplicates_from_sorted_array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def remove_duplicates(self, nums : list[int]) -> int:
+        k = sorted(list(set(nums)))
+        nums.clear()
+        nums += k
+        return len(k)
+
+solution = Solution()
+
+test_case_1 = [1,1,2]
+test_case_2 = [0,0,1,1,1,2,2,3,3,4]
+
+print(solution.remove_duplicates(test_case_1))
+print(solution.remove_duplicates(test_case_2))
