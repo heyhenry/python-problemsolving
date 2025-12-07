71550ab1aa52d5262717f41c6db0d85e472eea54
heyhenry <henryyy3@hotmail.com>
2025-12-07 15:08:19 +0800

    Leetcode: Solved no. 13 Roman to Integer.

---
+++ 2025/November/leetcode/roman_to_integer.py
@@ -0,0 +1,45 @@
+class Solution:
+    def roman_to_int(self, s : str) -> int:
+        roman_numerals = {
+            "I": 1,
+            "V": 5,
+            "IV": 4,
+            "IX": 9,
+            "X": 10,
+            "XL": 40,
+            "L": 50,
+            "XC": 90,
+            "C": 100,
+            "CD": 400,
+            "D": 500,
+            "CM": 900,
+            "M": 1000
+        }
+
+        result = 0
+        skip_flag = False
+        for i in range(len(s)):
+            if skip_flag:
+                skip_flag = False
+                continue
+            if i < len(s)-1:
+                if s[i] + s[i+1] in roman_numerals:
+                    result += roman_numerals[s[i]+s[i+1]]
+                    if i+1 == len(s)-1:
+                        return result
+                    skip_flag = True
+                else:
+                    result += roman_numerals[s[i]]
+            else:
+                result += roman_numerals[s[i]]
+        return result
+
+solution = Solution()
+
+test_case_1 = "III"
+test_case_2 = "LVIII"
+test_case_3 = "MCMXCIV"
+
+print(solution.roman_to_int(test_case_1))
+print(solution.roman_to_int(test_case_2))
+print(solution.roman_to_int(test_case_3))
