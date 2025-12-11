47ebe8ac32a3722815910a8a6c4977a206d0fa33
heyhenry <henryyy3@hotmail.com>
2025-12-11 19:31:24 +0800

    Leetcode: Solved no.1 Two Sum.

---
+++ 2025/December/Leetcode/two_sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def two_sum(self, nums : list[int], target: int) -> list[int]:
+        size = len(nums)
+        for i in range(size):
+            for j in range(i+1, size):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return
+
+solution = Solution()
+
+test_case_1 = ([2,7,11,15], 9)
+test_case_2 = ([3,2,4], 6)
+test_case_3 = ([3,3], 6)
+
+print(solution.two_sum(test_case_1[0], test_case_1[1]))
+print(solution.two_sum(test_case_2[0], test_case_2[1]))
+print(solution.two_sum(test_case_3[0], test_case_3[1]))
