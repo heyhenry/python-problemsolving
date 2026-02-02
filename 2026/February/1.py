3d72b88548360a22c5d1f764892cd41558379c42
heyhenry <henryyy3@hotmail.com>
2026-02-02 09:07:00 +0800

    Leetcode: solved no.1 Two Sum (2 Pass Hash method)

--- 2026/February/1.py
+++ 2026/February/1.py
@@ -1,12 +1,27 @@
 class Solution:
+    # brute force method
+    # def twoSum(self, nums: list[int], target: int) -> list[int]:
+    #     size = len(nums)
+    #     for i in range(size):
+    #         for j in range(i+1, size):
+    #             if nums[i] + nums[j] == target:
+    #                 # log result for checking
+    #                 print([i, j])
+    #                 return [i, j]
+
+    # two hash map pass method
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         size = len(nums)
+        nums_map = {}
+
         for i in range(size):
-            for j in range(i+1, size):
-                if nums[i] + nums[j] == target:
-                    # log result for checking
-                    print([i, j])
-                    return [i, j]
+            nums_map[nums[i]] = i
+        
+        for i in range(size):
+            complement = target - nums[i]
+            if complement in nums_map and nums_map[complement] != i:
+                print(i, [nums_map[complement]])
+                return [i, nums_map[complement]]
 
 test_case_01 = ([2,7,11,15], 9)
 test_case_02 = ([3,2,4], 6)
