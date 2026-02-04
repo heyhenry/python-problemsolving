9b27d69feff13f5a94c155d0c7b058a4b6783a59
heyhenry <henryyy3@hotmail.com>
2026-02-05 04:59:36 +0800

    Solved leetcode no.125 valid palindrome using a method with less lines (list comprehension & join()).

--- 2026/February/Leetcode/125.py
+++ 2026/February/Leetcode/125.py
@@ -2,15 +2,23 @@
 # More Info: https://leetcode.com/problems/valid-palindrome/description/
 
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        new_s = ""
-        for c in s:
-            if c.isalnum():
-                new_s += c.lower()
-        # to log result
-        print(new_s == new_s[::-1])    
-        return new_s == new_s[::-1]
+    # method 1
+    # def isPalindrome(self, s: str) -> bool:
+    #     new_s = ""
+    #     for c in s:
+    #         if c.isalnum():
+    #             new_s += c.lower()
+    #     # to log result
+    #     print(new_s == new_s[::-1])    
+    #     return new_s == new_s[::-1]
     
+    # method 2 (just less lines)
+    def isPalindrome(self, s: str) -> bool:
+        s = [c.lower() for c in s if c.isalnum()]
+        s = "".join(s)
+        print(s == s[::-1])
+        return s == s[::-1]
+
 test_case_01 = "A man, a plan, a canal: Panama"
 test_case_02 = "race a car"
 test_case_03 = " "
