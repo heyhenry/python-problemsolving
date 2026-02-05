7469d85f342be02e5b53f79a60a402c892038160
heyhenry <henryyy3@hotmail.com>
2026-02-05 10:02:24 +0800

    Leetcode: Added comments for personal clarification of no.15's process.

--- 2026/February/Leetcode/15.py
+++ 2026/February/Leetcode/15.py
@@ -9,6 +9,9 @@ class Solution:
         size = len(nums)
         
         for i in range(size):
+            # Checks the values of the indexes i and the previous one starting for duplicate values, 
+            # which will lead to an automatic skip of the index iteration at hand.
+            # It will start checking the conditions of the index's after the first iteration.
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             
@@ -16,15 +19,22 @@ class Solution:
             right = size-1
             
             while left < right:
+                # Provides the initial summed total to be used for comparison and 2 pointer adjustment.
                 total_sum = nums[i] + nums[left] + nums[right]
 
+                # Adjust the required pointer (left or right) based on the value of the summed total.
                 if total_sum < 0:
                     left += 1
                 elif total_sum > 0:
                     right -= 1
                 else:
+                    # Stores the valid triplet in the results list
                     results.append([nums[i], nums[left], nums[right]])
+                    # Will continue to the next element moving from the left index.
                     left += 1
+                    # If a duplicate value is found, then it will skip and continue going
+                    # through the elements until a non-duplicate value is found,
+                    # then it will break the inner while loop and resume the outer while loop.
                     while left < right and nums[left] == nums[left-1]: 
                         left += 1
         print(results)
