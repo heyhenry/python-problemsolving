80d3c79cc54d76aa63b0a7ffed4e70c755e01248
heyhenry <henryyy3@hotmail.com>
2026-02-06 11:29:00 +0800

    Leetcode: solved(kinda) no.121 Best Time to Buy and Sell Stock (but exceeds time limit on leetcode testing).

---
+++ 2026/February/Leetcode/121.py
@@ -0,0 +1,23 @@
+# Leetcode No.121: Best Time to Buy and Sell Stock
+# More Info: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        size = len(prices)
+        best_price = 0
+        for i in range(size):
+            for j in range(i+1, size):
+                profit = prices[j] - prices[i]
+                if profit > best_price:
+                    best_price = profit
+        print(best_price)
+        return best_price
+
+test_case_01 = [7,1,5,3,6,4]
+test_case_02 = [7,6,4,3,1]
+
+s = Solution()
+
+s.maxProfit(test_case_01)
+# s.maxProfit(test_case_02)
+
