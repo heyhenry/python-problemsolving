4d2d48afe94d3d699c2f1e7349fe66c9a795c124
heyhenry <henryyy3@hotmail.com>
2026-02-09 11:35:43 +0800

    Leetcode: solved no.704 Binary Search (using the binary search technique)

--- 2026/February/Leetcode/704.py
+++ 2026/February/Leetcode/704.py
@@ -2,11 +2,32 @@
 # More Info: https://leetcode.com/problems/binary-search/description/
 
 class Solution:
+    # def search(self, nums: list[int], target: int) -> int:
+    #     for i in range(len(nums)):
+    #         if nums[i] == target:
+    #             print(i)
+    #             return i
+    #     print(-1)
+    #     return -1
+
+    # the binary search method way
     def search(self, nums: list[int], target: int) -> int:
-        for i in range(len(nums)):
-            if nums[i] == target:
-                print(i)
-                return i
+        size = len(nums)
+        left = 0
+        right = size-1
+        # continue loop while left's value is less than or equal to right's value
+        while left <= right:
+            # find the middle of the list
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                print(mid)
+                return mid
+            # find the new left value, which will be used in the next iteration for the new middle
+            elif nums[mid] < target:
+                left = mid + 1
+            # find the new right value, which will be used in the next iteration for the new middle
+            elif nums[mid] > target:
+                right = mid - 1
         print(-1)
         return -1
 
