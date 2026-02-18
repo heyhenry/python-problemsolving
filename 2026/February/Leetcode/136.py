5cb429e0257635bed3d5a2ec0be1527d593ce5fd
heyhenry <henryyy3@hotmail.com>
2026-02-18 23:57:33 +0800

    Leetcode: solved no.136 Single Number (using dict and list to count and filter the single number)

--- 2026/February/Leetcode/136.py
+++ 2026/February/Leetcode/136.py
@@ -7,6 +7,22 @@ class Solution:
         for i in unique_nums:
             if nums.count(i) == 1:
                 return i
+            
+    def singleNumber(self, nums: list[int]) -> int:
+        nums_dict = {}
+        smallest_number = []
+        for i in nums:
+            if i not in nums_dict:
+                nums_dict[i] = 1
+            else:
+                nums_dict[i] += 1
+            if nums_dict[i] == 1:
+                smallest_number.append(i)
+            else:
+                smallest_number.remove(i)
+        
+        print(smallest_number[0])
+        return smallest_number[0]
 
 test_case_01 = [2,2,1]
 test_case_02 = [4,1,2,1,2]
