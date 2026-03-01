5e8510a1348fa06fcaadbde6a7181214f9a06623
heyhenry <henryyy3@hotmail.com>
2026-03-02 06:09:46 +0800

    Leetcode: Solved no.1: Two Sum (using one pass method)

--- 2026/March/Leetcode/1.py
+++ 2026/March/Leetcode/1.py
@@ -2,11 +2,22 @@
 # More Info: https://leetcode.com/problems/two-sum/description/
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        size = len(nums)
-        for i in range(size):
-            for j in range(i+1, size):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+
+        # method: brute force
+        # size = len(nums)
+        # for i in range(size):
+        #     for j in range(i+1, size):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+
+        # method: one pass
+        num_map = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[nums[i]] = i
 
 test_case_01 = ([2,7,11,15], 9)
 test_case_02 = ([3,2,4], 6)
