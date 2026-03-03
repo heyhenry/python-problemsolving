58671b9d84cb9eb65caa1f810be2e7f2eb3cd4c9
heyhenry <henryyy3@hotmail.com>
2026-03-04 00:33:39 +0800

    Leetcode: no.217 learnt faster runtime via code tweak.

--- 2026/March/Leetcode/217.py
+++ 2026/March/Leetcode/217.py
@@ -3,9 +3,13 @@
 
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
-        unique_nums = set(nums)
-        return len(unique_nums) != len(nums)
+        # method 1: using set()
+        # unique_nums = set(nums)
+        # return len(unique_nums) != len(nums)
 
+        # tested and learnt that writing the above code like this reads a faster runtime.
+        return len(set(nums)) != len(nums)
+        
 test_case_01 = [1,2,3,1]
 test_case_02 = [1,2,3,4]
 test_case_03 = [1,1,1,3,3,4,3,2,4,2]
