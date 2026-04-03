edb8661ec615ec61f014627139091c5a9308c531
heyhenry <henryyy3@hotmail.com>
2026-04-03 14:40:02 +0800

    PSET: Alternative solution for expansion in pset02.

--- 2026/April/Problem_Sets/Pset_02/expansion.py
+++ 2026/April/Problem_Sets/Pset_02/expansion.py
@@ -6,11 +6,21 @@ expansion('') -> []
 expansion('abccba') -> ['a', 'ab', 'abc', 'abcc', 'abccb', 'abccba']
 """
 def expansion(s : str) -> list[str]:
-    results = []
-    expanding_s = ""
-    for c in s:
-        expanding_s += c
-        results.append(expanding_s)
+    # solution 1
+    # results = []
+    # expanding_s = ""
+    # for c in s:
+    #     expanding_s += c
+    #     results.append(expanding_s)
+    # return results
+
+    # solution 2
+    if len(s) < 2:
+        return [s]
+
+    results = [s[0]]
+    for c in range(1, len(s)):
+        results.append(results[-1]+s[c])
     return results
 
 print(expansion('test'))
