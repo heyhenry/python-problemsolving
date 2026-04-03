bc65325531965f9e0574e0ed72d4e06648193417
heyhenry <henryyy3@hotmail.com>
2026-04-03 20:11:17 +0800

    PSET: Solved count_letters in pset02.

---
+++ 2026/April/Problem_Sets/Pset_02/count_letters.py
@@ -0,0 +1,27 @@
+""" 
+  Returns the count of each letter in `s` as a dictionary 
+ 
+  Examples (dictionary print order doesn't matter): 
+    count_letters("") -> {} 
+    count_letters("abc") -> {'a' : 1, 'b' : 1, 'c' : 1} 
+    count_letters("aaaa") -> {'a' : 4} 
+    count_letters("ababbcca") -> {'a' : 3, 'b' : 3, 'c' : 2} 
+"""
+def count_letters(s : str) -> dict[str, int]: 
+    letters = {}
+    for c in s:
+        if c not in letters:
+            letters[c] = 1
+        else:
+            letters[c] += 1
+    return letters
+
+test_cases = [
+    "",
+    "abc",
+    "aaaa",
+    "ababbcca"
+]
+
+for i in test_cases:
+    print(count_letters(i))
