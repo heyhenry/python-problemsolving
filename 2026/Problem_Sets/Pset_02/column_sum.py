51cbcba3b2d497f38b735c509da91f8b64096a94
heyhenry <henryyy3@hotmail.com>
2026-04-03 20:22:12 +0800

    Problem Sets: Updated comment.

--- 2026/Problem_Sets/Pset_02/column_sum.py
+++ 2026/Problem_Sets/Pset_02/column_sum.py
@@ -2,8 +2,7 @@
 Returns the sum of each column of the _rectangular_ matrix `mat`
 (rectangular means each row is the same length)
 Examples:
-column_sum([[1, 2, 3], [4, 5, 6]]) -> [5, 7, 9] # yes, I did it right this
-time
+column_sum([[1, 2, 3], [4, 5, 6]]) -> [5, 7, 9]
 column_sum([[], []]) -> [0]
 column_sum([[2, 2], [-1, -2], [3, 9]]) -> [4, 9]
 """
