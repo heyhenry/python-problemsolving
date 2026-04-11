887431b2530d6ef445c8d210c60b434a41959d78
heyhenry <henryyy3@hotmail.com>
2026-04-12 05:45:48 +0800

    PSET: Solved matrix_multiply in pset02.

--- 2026/April/Problem_Sets_Again_Again/Pset_02/matrix_multiply.py
+++ 2026/April/Problem_Sets_Again_Again/Pset_02/matrix_multiply.py
@@ -11,7 +11,24 @@ Examples:
     https://matrix.reshish.com/multiplication.php <-- working reference 
 """
 def matrix_multiply(mat1 : list[list[int]], mat2 : list[list[int]]) -> list[list[int]]:
-    pass
+    mat1_rows = len(mat1)
+    mat1_cols = len(mat1[0])
+    mat2_rows = len(mat2)
+    mat2_cols = len(mat2[0])
+
+    results = []
+    for _ in range(mat1_rows):
+        temp = []
+        for _ in range(mat2_cols):
+            temp.append(0)
+        results.append(temp)
+    
+    for i in range(mat2_cols):
+        for j in range(mat1_rows):
+            for k in range(mat1_cols):
+                results[i][j] += mat1[i][k] * mat2[k][j]
+                
+    return results
 
 test_cases = [
     ([[1, 2], [3, 4]], [[1, 2], [3, 4]]),
