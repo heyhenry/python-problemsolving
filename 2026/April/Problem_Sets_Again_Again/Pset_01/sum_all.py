001dcd8df8ad15dfbde201680a63616019572b43
heyhenry <henryyy3@hotmail.com>
2026-04-12 05:30:05 +0800

    PSET: Updated sum_all in pset01.

--- 2026/April/Problem_Sets_Again_Again/Pset_01/sum_all.py
+++ 2026/April/Problem_Sets_Again_Again/Pset_01/sum_all.py
@@ -3,7 +3,7 @@ Returns the sum of each row of the table as a list
 Examples: 
     sum_all([[1, 2, 3], [4, 5, 6]]) -> [6, 15] 
     sum_all([[2], [4, 7], [-1, -2, -4, -5]]) -> [2, 11, -12] 
-    sum_all([[]]) -> [] 
+    sum_all([[]]) -> [0] 
 """
 def sum_all(table : list[list[int]]) -> list[int]: 
     results = []
