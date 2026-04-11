f580667ebd4ef38124dff5e78aa44112eb6efbf7
heyhenry <henryyy3@hotmail.com>
2026-04-12 05:52:32 +0800

    PSET: Solved row_sum in pset02.

--- 2026/April/Problem_Sets_Again_Again/Pset_02/row_sum.py
+++ 2026/April/Problem_Sets_Again_Again/Pset_02/row_sum.py
@@ -7,7 +7,15 @@ row_sum([[-1, -5, 6], [7, 2], [10, 10, 10]]) -> [[-1, -6, 0], [7, 9], [10, 20,
 30]]
 """
 def row_sum(table : list[list[int]]) -> list[list[int]]:
-    pass
+    results = []
+    for row in table:
+        num = 0
+        temp = []
+        for i in row:
+            num += i
+            temp.append(num)
+        results.append(temp)
+    return results
 
 print(row_sum([[1, 2, 3], [4, 5, 6]]))
 print(row_sum([[], [4], [], [5]]))
