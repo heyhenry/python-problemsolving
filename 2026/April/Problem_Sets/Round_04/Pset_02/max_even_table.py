65a28f25ac2bdbffa54af4f09eec6c0b32ee98ab
heyhenry <henryyy3@hotmail.com>
2026-04-14 09:05:45 +0800

    PSET: Solved max_even_table in pset02.

---
+++ 2026/April/Problem_Sets/Round_04/Pset_02/max_even_table.py
@@ -0,0 +1,23 @@
+"""
+Returns the maximum even number in each row of `lst`, or `None` if none exists
+Note that negatives will be negative when the associated positive is
+Examples:
+max_even_table([[1, 2, 3]]) -> [2]
+max_even([[-4, 3, -1, -6], [1, 3, 5]]) -> [-4, None]
+max_even_table([[], [1, 4, 6, 2], [-1, -2]]) -> [None, 6, -2]
+"""
+def max_even_table(lst : list[list[int]]) -> list[int | None]:
+    results = []
+    for row in lst:
+        max_even_val = None
+        for i in row:
+            if i % 2 == 0 and max_even_val is None:
+                max_even_val = i
+            elif i % 2 == 0 and i > max_even_val:
+                max_even_val = i
+        results.append(max_even_val)
+    return results
+
+print(max_even_table([[1, 2, 3]]))
+print(max_even_table([[-4, 3, -1, -6], [1, 3, 5]]))
+print(max_even_table([[], [1, 4, 6, 2], [-1, -2]]))
