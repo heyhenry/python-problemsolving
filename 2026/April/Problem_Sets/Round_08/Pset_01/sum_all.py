c9160ebe3393f5a59c008f3c86ab003da99c7619
heyhenry <henryyy3@hotmail.com>
2026-04-18 00:01:44 +0800

    PSET: Solved sum_all in pset01.

---
+++ 2026/April/Problem_Sets/Round_08/Pset_01/sum_all.py
@@ -0,0 +1,19 @@
+""" 
+Returns the sum of each row of the table as a list 
+Examples: 
+    sum_all([[1, 2, 3], [4, 5, 6]]) -> [6, 15] 
+    sum_all([[2], [4, 7], [-1, -2, -4, -5]]) -> [2, 11, -12] 
+    sum_all([[]]) -> [0] 
+"""
+def sum_all(table : list[list[int]]) -> list[int]: 
+    results = []
+    for row in table:
+        val = 0
+        for i in row:
+            val += i
+        results.append(val)
+    return results
+
+print(sum_all([[1, 2, 3], [4, 5, 6]]))
+print(sum_all([[2], [4, 7], [-1, -2, -4, -5]]))
+print(sum_all([[]]))
