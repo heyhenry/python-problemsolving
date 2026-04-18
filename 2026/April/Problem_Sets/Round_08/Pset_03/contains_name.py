7469677da8fda9a921a31b4ba897c563b25138c8
heyhenry <henryyy3@hotmail.com>
2026-04-18 10:03:18 +0800

    PSET: Solved contains_name in pset03.

---
+++ 2026/April/Problem_Sets/Round_08/Pset_03/contains_name.py
@@ -0,0 +1,43 @@
+""" 
+Given a filename ending with `csv` 
+
+If that filecontains a `name` column, returns if `name` is in that column 
+
+Examples: 
+    contains_name("table1.csv", "goblin") -> True 
+    contains_name("table1.csv", "Eve") -> False // uppercase 
+    contains_name("table2.csv", "bob") -> False // no name column 
+    contains_name("table3.csv", "Taylor Swift") -> True 
+    contains_name("table3.csv", "Sabaton") -> False 
+"""
+def contains_name(filename : str, name : str) -> bool:
+    with open(filename, "r") as file:
+        content = file.read().splitlines()
+        updated_content = []
+        for row in content:
+            temp = []
+            for i in row.split(','):
+                temp.append(i)
+            updated_content.append(temp)
+        categories = updated_content[0]
+        updated_content.pop(0)
+        name_index = None
+        for col in range(len(categories)):
+            if categories[col] == "name":
+                name_index = col
+        if name_index is not None:
+            for row in updated_content:
+                if row[name_index] == name:
+                    return True
+        return False
+
+test_cases = [
+    ("table_data/table1.csv", "goblin"),
+    ("table_data/table1.csv", "Eve"),
+    ("table_data/table2.csv", "bob"),
+    ("table_data/table3.csv", "Taylor Swift"),
+    ("table_data/table3.csv", "Sabaton")
+]
+
+for case in test_cases:
+    print(contains_name(case[0], case[1]))
