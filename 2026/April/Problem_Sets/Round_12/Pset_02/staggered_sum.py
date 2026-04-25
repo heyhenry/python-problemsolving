33a0dca4144efe3199367b5b022ad6ebcb43bd77
heyhenry <henryyy3@hotmail.com>
2026-04-25 10:27:56 +0800

    PSET: Solved staggered_sum in pset02.

---
+++ 2026/April/Problem_Sets/Round_12/Pset_02/staggered_sum.py
@@ -0,0 +1,22 @@
+"""
+Returns the sum of elements in `lst`, which is a list of integers _or_ lists
+Examples:
+staggered_sum([1, 2, 3]) -> 6
+staggered_sum([[], [], []]) -> 0
+staggered_sum([[1, 2], 3, [4, 5]]) -> 15
+staggered_sum([-4, [-3, 2, 1], 6, 8, [-1]]) -> 9
+"""
+def staggered_sum(lst : list[int | list[int]]) -> int:
+    result = 0
+    for elem in lst:
+        if isinstance(elem, list):
+            for i in elem:
+                result += i
+        else:
+            result += elem
+    return result
+
+print(staggered_sum([1, 2, 3]))
+print(staggered_sum([[], [], []]))
+print(staggered_sum([[1, 2], 3, [4, 5]]))
+print(staggered_sum([-4, [-3, 2, 1], 6, 8, [-1]]))
