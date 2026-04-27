58d942bccb45f6aa3131c27a659c8482122717ef
heyhenry <henryyy3@hotmail.com>
2026-04-27 10:48:26 +0800

    PSET: Solved expansion in pset02.

---
+++ 2026/April/Problem_Sets/Round_14/Pset_02/expansion.py
@@ -0,0 +1,18 @@
+"""
+Returns the list of each letter of `s` expanded in sequence
+Examples:
+expansion('test') -> ['t', 'te', 'tes', 'test']
+expansion('') -> []
+expansion('abccba') -> ['a', 'ab', 'abc', 'abcc', 'abccb', 'abccba']
+"""
+def expansion(s : str) -> list[str]:
+    new_s = ""
+    results = []
+    for c in s:
+        new_s += c
+        results.append(new_s)
+    return results
+
+print(expansion('test'))
+print(expansion(''))
+print(expansion('abccba'))
