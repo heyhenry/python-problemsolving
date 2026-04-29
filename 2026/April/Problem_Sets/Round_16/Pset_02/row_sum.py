82f84723559ce7493975ccd4f5bb46c4109aacc4
heyhenry <henryyy3@hotmail.com>
2026-04-29 13:38:54 +0800

    PSET: Solved row_sum in pset02.

---
+++ 2026/April/Problem_Sets/Round_16/Pset_02/row_sum.py
@@ -0,0 +1,22 @@
+"""
+Returns the sum up to each element of the given table
+Examples:
+row_sum([[1, 2, 3], [4, 5, 6]]) -> [[1, 3, 6], [4, 9, 15]]
+row_sum([[], [4], [], [5]]) -> [[], [4], [], [5]]
+row_sum([[-1, -5, 6], [7, 2], [10, 10, 10]]) -> [[-1, -6, 0], [7, 9], [10, 20,
+30]]
+"""
+def row_sum(table : list[list[int]]) -> list[list[int]]:
+    results = []
+    for row in table:
+        val = 0
+        temp = []
+        for i in row:
+            val += i
+            temp.append(val)
+        results.append(temp)
+    return results
+
+print(row_sum([[1, 2, 3], [4, 5, 6]]))
+print(row_sum([[], [4], [], [5]]))
+print(row_sum([[-1, -5, 6], [7, 2], [10, 10, 10]]))
