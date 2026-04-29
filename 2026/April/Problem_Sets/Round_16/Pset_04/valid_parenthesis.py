13b3a948f18f62d959c1a71b10be4b2a93d7ede7
heyhenry <henryyy3@hotmail.com>
2026-04-29 14:03:36 +0800

    PSET: Sovled valid_parenthesis in pset04.

---
+++ 2026/April/Problem_Sets/Round_16/Pset_04/valid_parenthesis.py
@@ -0,0 +1,33 @@
+"""
+1. Valid Parentheses
+Your task is to determine if a given collection of parentheses are "valid", meaning that an opening parenthesis
+or brace is matched directly with the associated parenthesis or brace.
+File format: a single line consisting only of (, [, ), or ]
+Output: either true or false
+"""
+def is_valid(filename : str) -> bool:
+    with open(filename, "r") as file:
+        content = file.read()
+        stack = []
+        p = {"]": "[", ")": "("}
+        for i in content:
+            if i in p.values():
+                stack.append(i)
+            elif stack and stack[-1] == p[i]:
+                stack.pop()
+            else:
+                return False
+        if stack:
+            return False
+        return True
+
+test_cases = [
+    "test_cases/1/input1.txt", # true
+    "test_cases/1/input2.txt", # false
+    "test_cases/1/input3.txt", # false
+    "test_cases/1/input4.txt", # true 
+    "test_cases/1/input5.txt" # false
+]
+
+for test in test_cases:
+    print(is_valid(test))
