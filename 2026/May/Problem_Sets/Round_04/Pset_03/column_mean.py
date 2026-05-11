942c94ccd513ccbf955387f8d71b880c1dad4674
heyhenry <henryyy3@hotmail.com>
2026-05-12 07:47:26 +0800

    PSET: Solved column_mean in pset03.

---
+++ 2026/May/Problem_Sets/Round_04/Pset_03/column_mean.py
@@ -0,0 +1,47 @@
+""" 
+Given a CSV filename, calculate the mean of each column in the CSV 
+
+Requires that filename end with `.csv` and the file contains only integers 
+
+Any empty row element is assumed to be 0 
+
+Examples: 
+    column_mean("data1.csv") -> [] 
+    column_mean("data2.csv") -> [3, -1, .5, 2] 
+    column_mean("data3.csv") -> [3, 1, 0] 
+    column_mean("data4.csv") -> [2.5, 3.5, 4.5] 
+"""
+def column_mean(filename : str) -> list[float]: 
+    with open(filename, "r") as file:
+        content = file.read()
+        if not content:
+            return []
+        content = content.splitlines()
+        updated_content = []
+        for row in content:
+            temp = []
+            for i in row.split(","):
+                temp.append(int(i))
+            updated_content.append(temp)
+        col_check = len(max(updated_content, key=len))
+        for row in updated_content:
+            while len(row) < col_check:
+                row.append(0)
+        results = []
+        for col in range(len(updated_content[0])):
+            val = 0
+            for row in range(len(updated_content)):
+                val += updated_content[row][col]
+            val = val / len(updated_content)
+            results.append(val)
+        return results
+
+test_cases = [
+    "csv_data/data1.csv",
+    "csv_data/data2.csv",
+    "csv_data/data3.csv",
+    "csv_data/data4.csv"
+]
+
+for csv in test_cases:
+    print(column_mean(csv))
