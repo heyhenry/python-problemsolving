eb11ceac1a4ce5059c9b1b9bdab5e82250a901df
heyhenry <henryyy3@hotmail.com>
2026-05-26 16:40:26 +0800

    PSET: Solved reverse_string in pset01.

---
+++ 2026/May/Problem_Sets/Round_07/Pset_01/reverse_string.py
@@ -0,0 +1,14 @@
+""" 
+  Returns the string `s` in reverse order 
+  Examples: 
+    reverse_string('abc') -> 'cba' 
+    reverse_string('') -> '' 
+    reverse_string('abab') -> 'baba' 
+"""
+def reverse_string(s : str) -> str:
+    s = s[::-1]
+    return s
+
+print(reverse_string('abc'))
+print(reverse_string(''))
+print(reverse_string('abab'))
