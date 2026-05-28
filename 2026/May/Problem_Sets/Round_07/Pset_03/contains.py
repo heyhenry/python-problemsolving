dab9ccfc9bb47f287cfae21397f4ad2d7956bf9e
heyhenry <henryyy3@hotmail.com>
2026-05-28 14:25:32 +0800

    PSET: Solved contains in pset03.

---
+++ 2026/May/Problem_Sets/Round_07/Pset_03/contains.py
@@ -0,0 +1,30 @@
+""" 
+Returns whether the given word appears in a `.fakeletters` file 
+
+Requires that `filename` ends with `.fakeletters` 
+ 
+Examples: 
+    contains("test1.fakeletters", "b") -> True 
+    contains("test1.fakeletters", "d") -> False 
+    contains("test2.fakeletters", "wor") -> False 
+    contains("test2.fakeletters", "hello") -> True 
+    contains("test3.fakeletters", "") -> False 
+"""
+def contains(filename : str, word : str) -> bool: 
+    with open(filename, "r") as file:
+        content = file.read()
+        if not content:
+            return False
+        return word in content.split(" ")
+
+if __name__ == "__main__":
+    test_cases = [
+        ("test1.fakeletters", "b"),
+        ("test1.fakeletters", "d"),
+        ("test2.fakeletters", "wor"),
+        ("test2.fakeletters", "hello"),
+        ("test3.fakeletters", ""),
+    ]
+
+    for i in range(len(test_cases)):
+        print(contains(f"fakeletters/{test_cases[i][0]}", test_cases[i][1]))
